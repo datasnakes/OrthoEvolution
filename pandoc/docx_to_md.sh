2aecfe9b25a509650a7751fba504caedc73e2175
robear22890 <robgilmore127@gmail.com>
2017-04-16 21:33:07 -0500

    Updated for custom functionality.
    Will now work on MCSR with file names that have white spaces.

--- pandoc/docx_to_md.sh
+++ pandoc/docx_to_md.sh
@@ -8,14 +8,29 @@
 # 4. Run the script
 
 # By default this will keep the original .md file
+# This script also removes white space from file names.
 
-FILES=*.docx
-for f in $FILES
+
+# White space removal
+FILES1=*.docx
+for f in $FILES1
+do
+  mv "$f" "${f//[[:space:]]}"
+  echo "Removed whitespace from $f"
+done
+
+# DOCX to MD conversion
+FILES2=*.docx
+for f in $FILES2
 do
   # extension="${f##*.}"
   filename="${f%.*}"
   echo "Converting $f to $filename.md"
-  `pandoc $f -t markdown -o $filename.md`
+  `pandoc "${f}" -t markdown -o $filename.md`
   # uncomment this line to delete the source file.
   # rm $f
 done
+
+# Recreates the original file
+for f in $FILES1
+do
