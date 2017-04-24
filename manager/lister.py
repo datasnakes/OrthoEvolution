9bd398f8d39b2efcfcff876adf2218c00a0a705c
rgilmore <rgilmore@umc.edu>
2017-04-24 17:44:52 -0500

    Cleaned;  Added PAML organisms, taxon_ids.

--- manager/lister.py
+++ manager/lister.py
@@ -34,7 +34,7 @@ from pathlib import Path
 
 import pandas as pd
 
-from dir_mana import dir_mana
+#from dir_mana import dir_mana
 
 ##############################################################################
 # Directory Initializations:
@@ -43,7 +43,7 @@ from dir_mana import dir_mana
 os.chdir(os.path.dirname(__file__))
 home = os.getcwd()
 project = "Orthologs-Project"
-where = dir_mana(home, project)
+#where = dir_mana(home, project)
 
 # Add a path that contains custom libraries for import
 # os.sys.path.append()
@@ -55,49 +55,79 @@ where = dir_mana(home, project)
 
 class Lister(object):
     __home = ''
-    __filename = ''
-    __filename_path = ''
+    __acc_filename = ''
+    __paml_filename = ''
+    __acc_path = ''
     __data = ''
     ##########################################################################
 
-    def __init__(self, csv_file, go_list=None, hgnc=False):
+    def __init__(self, acc_file=None, paml_file=None, taxon_file=None, go_list=None, hgnc=False):
 
         # Private Variables
         self.__home = home
-        self.__filename = csv_file
-        self.__filename_path = 'C:\\Users\\rgilmore\\PycharmProjects\\Orthologs-Project\\' / Path(self.__filename)
 
-        self.go_list = go_list
-        # Handles for organism lists #
-        self.org_list = []
-        self.org_count = 0
-        # Handles for gene lists #
-        self.gene_list = []
-        self.gene_count = 0
-        # Handles for tier lists #
-        self.tier_list = []
-        self.tier_dict = {}
-        self.tier_frame_dict = {}
-        # Handles for accession lists #
-        self.acc_dict = {}
-        self.acc_list = []
-        # Handles for master data structures #
-        self.__data = pd.read_csv(self.__filename_path)
-        self.header = self.__data.axes[1].tolist()
-        # #### Format the main data frame #### #
-        self.__data = self.__data.set_index('Gene')
-        self.df = self.__data
-        # #### Format the main pivot table #### #
-        self.pt = pd.pivot_table(pd.read_csv(self.__filename_path), index=['Tier', 'Gene'], aggfunc='first')
-        array = self.pt.axes[1].tolist()
-        self.pt.columns = pd.Index(array, name='Organism')
-        # #### Handles for full dictionaries #### #
-        self.org_dict = self.df.ix[0:, 'Homo_sapiens':].to_dict()
-        self.gene_dict = self.df.T.to_dict()
-        self.get_master_lists(self.__data)  # populates our lists
+        # Handle the paml organism file
+        if paml_file is not None:
+            # File init
+            self.__paml_filename = paml_file
+            self.__paml_path = Path(home) / Path('index') / Path(self.__paml_filename)
+            # Get PAML list
+            self.paml_org_list = self.get_file_list(self.__paml_path)
 
-        # TODO-ROB Add HGNC python module
+        # Handle the taxon_id file
+        if taxon_file is not None:
+            # File init
+            self.__taxon_filename = taxon_file
+            self.__taxon_path = Path(home) / Path('index') / Path(self.__taxon_filename)
+            # Get taxon id list
+            self.taxon_ids = self.get_file_list(self.__taxon_path)
 
+        # Handle the master accession file
+        if acc_file is not None:
+            # File init
+            self.__acc_filename = acc_file
+            self.__acc_path = Path(home) / Path('index') / Path(self.__acc_filename)
+            self.go_list = go_list
+            # Handles for organism lists #
+            self.org_list = []
+            self.org_count = 0
+            # Handles for gene lists #
+            self.gene_list = []
+            self.gene_count = 0
+            # Handles for tier lists #
+            self.tier_list = []
+            self.tier_dict = {}
+            self.tier_frame_dict = {}
+            # Handles for accession lists #
+            self.acc_dict = {}
+            self.acc_list = []
+            # Handles for master data structures #
+            self.__data = pd.read_csv(self.__acc_path)
+            self.header = self.__data.axes[1].tolist()
+
+            # #### Format the main data frame #### #
+            self.__data = self.__data.set_index('Gene')
+            self.df = self.__data
+
+            # #### Format the main pivot table #### #
+            self.pt = pd.pivot_table(pd.read_csv(self.__acc_path), index=['Tier', 'Gene'], aggfunc='first')
+            array = self.pt.axes[1].tolist() # Organism list
+            self.pt.columns = pd.Index(array, name='Organism')
+
+            # #### Handles for full dictionaries #### #
+            self.org_dict = self.df.ix[0:, 'Homo_sapiens':].to_dict()
+            self.gene_dict = self.df.T.to_dict()
+            self.get_master_lists(self.__data)  # populates our lists
+
+
+# TODO-ROB Add HGNC python module
+    @staticmethod
+    def get_file_list(file):
+        file_list = []
+        with open(file, 'r') as f:
+            for item in f.readline():
+                file_list.append(item.replace('\n', ''))
+        return file_list
     def get_accession(self, gene, organism):
         """Takes a single gene and organism and returns
         a single accession number."""
@@ -111,14 +141,12 @@ class Lister(object):
         Or it takes an empty gene/organisms list, which returns the 
         entire list of accession numbers."""
         if go_list is None:
-            go_list = self.go_list
-        maf = self.df
-        genes = []
-        organisms = []
-        for gene, organism in go_list:
-            genes.append(gene)
-            organisms.append(organism)
-        accessions = maf.lookup(genes, organisms).tolist()
+            accessions = self.acc_list
+        else:
+            accessions = []
+            for gene, organism in go_list:
+                accession = self.get_accession(gene, organism)
+                accessions.append(accession)
         return accessions
 
     def get_accession_alignment(self, gene):
@@ -147,7 +175,6 @@ class Lister(object):
         which contains the corresponding genes/organisms."""
         gene_list = self.gene_list
         org_list = self.org_list
-        maf = self.df
         go1 = []
         go = {}
         for gene in gene_list:
@@ -159,11 +186,13 @@ class Lister(object):
                 go1 = []
         return go
 
-    def get_master_lists(self, df):
+    def get_master_lists(self, df=None, csv_file=None):
         """This function populates the organism and gene lists with a data frame.
         This function also populates several dictionaries.
         The dictionaries contain separate keys for Missing genes."""
-
+        if csv_file is not None:
+            self.__init__(csv_file)
+            df = self.df
         maf = df
         self.gene_list = maf.index.tolist()
         self.gene_count = len(self.gene_list)
