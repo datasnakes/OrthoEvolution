651ef5704ff8da6f7e9d7d3fab2cab57b226faaa
rgilmore <rgilmore@umc.edu>
2017-05-12 17:01:33 -0500

    Created an archive of this file;  Removed deprecated methods;  Added docstrings.

--- Manager/utils/mana.py
+++ Manager/utils/mana.py
@@ -31,8 +31,9 @@ import Manager
 import Orthologs
 import Tools
 # from Manager.logit.logit import LogIt
-##############################################################################
-# Directory Initializations:
+
+# datasnakes~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# datasnakes~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 
 class Mana(object):
@@ -48,14 +49,13 @@ class Mana(object):
 
     def __init__(self, repo=None, home=os.getcwd(), new_repo=False):
         # TODO-ROB ADD a REPOsitory destination path (an output directory for cookiecutter)
-        self.repo = repo
         """
         :param home(path or path-like): The home of the file calling this name.  When creating a new 
             repository it is best to explicitly name the home path.
         :param repo(string): The name of the new repository to be created.
         :param new_repo(bool): Triggers cookiecutter to create a new repository.
         """
-        # config = tablib.Dataset().load(open('config.yaml').read())
+        self.repo = repo
         self.file_home = Path(home)  # Home of the file calling this class
         # TODO-ROB:  SOme of these directories don't need to be accessed directly
         # Below are the PyPi path strings
@@ -177,69 +177,25 @@ class Mana(object):
         return t
 
     # DEPRECATED Change this IN OTHER CLASSES
-    def path_list_make(self, path, o_path=None):
+   # def path_list_make(self, path, o_path=None):
         # Takes a path and reduces it to a list of directories within the project
         # An optional attribute (o_path) is give so that a deeper path within the project can be used
-        home = str(self.__project_home).split('/')
-        path_list = str(path).split('/')
-        for item in home:
-            if item in path_list:
-                path_list.remove(item)
-        # path_list = set(p) - set(home)
-        if o_path is not None:
-            o_path = str(o_path).split('/')
-            for item in o_path:
-                if item in path_list:
-                    path_list.remove(item)
-            # path_list = set(path_list) - set(o_path)
-        return path_list
 
     # //TODO-ROB utilize Path.mkdir(parents=TRUE) instead
         # DEPRECATED Change this IN OTHER CLASSES
-    def dir_make(self, path, path_list):
+  #  def dir_make(self, path, path_list):
         # Takes a path list which is a list of folder names
         # path_list created by str(path).split('/')
         # The path_list appends to path, which is already an established directory inside the project
-        t = None
-        for item in path_list:
-
-            if os.path.isdir(path + '/' + item): # If for some reason the directory already exists...
-                path += '/' + item  # Append a directory
-                continue
-            path += '/' + item  # Append a directory
-            os.mkdir(path)
-        if len(os.listdir(path)) > 0:
-            path, t = self.dir_archive(path, path_list='')
-        return path, t
 
     # # //TODO-ROB Change to using a compression module https://pymotw.com/2/compression.html
         # DEPRECATED Change this IN OTHER CLASSES
     # def dir_archive(self, path, path_list):
     #     # Use the path that you want to update/add to
     #     # Returns path and the time stamp (could be None)
-    #     unique_dir = False
-    #     archive_path = path
-    #     for item in path_list:
-    #
-    #         path += '/' + item  # Append a directory
-    #         if os.path.isdir(path):  # If the child directory exists
-    #             archive_path = path  # Then update the dir_archive path and continue
-    #             continue
-    #         else:                     # If the child directory doesnt exist
-    #             unique_dir = True     # Then raise the flag
-    #             os.mkdir(path)        # And make a directory
-    #
-    #     if unique_dir is False:  # Only dir_archive if the final child directory is not unique (via unique_dir = False)
-    #         t = time.strftime("%m%d%Y-%I%M%S")
-    #         new_archive = self.Archive + '/' + t  # Creates a time stamped directory
-    #
-    #         os.mkdir(new_archive)
-    #         for item in os.listdir(archive_path):
-    #             if os.path.isfile(archive_path + '/' + item):  # Only dir_archive the FILES
-    #                 shutil.move(archive_path + '/' + item, new_archive)
-    #         return path, t
-    #     else:
-    #         return path, None
+
+# datasnakes~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# datasnakes~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 
 class RepoMana(Mana):
@@ -250,7 +206,7 @@ class RepoMana(Mana):
         :param user (string):  The name of the current user if any. 
         :param home (string or pathlike):  The home path of the repository.
         :param new_user (bool):  Flag for creating a new user. 
-        :param new_repo: 
+        :param new_repo: Flag for creating a new repository.
         """
         # TODO-ROB change the home parameter to the output directory parameter
         super().__init__(repo=repo, home=home, new_repo=new_repo)
@@ -288,8 +244,10 @@ class RepoMana(Mana):
         cookiecutter(self.user_cookie, no_input=True, extra_context={"user_name": self.user}, output_dir=self.users)
         # TODO-ROB do we need create user hooks?
 
-
+# datasnakes~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# datasnakes~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # TODO-ROB:  Edit the setup.py file for cookiecutter.
+
 
 class UserMana(RepoMana):
     # TODO-ROB CREATE THESE IN A VIRTUAL ENVIRONMENT FOR EACH USER
@@ -297,6 +255,19 @@ class UserMana(RepoMana):
     # TODO-ROB When the user logs in, they will activate the virtual environment
     # TODO-ROB USE SQL here to see if the user db contains the username
     def __init__(self, repo, user, project=None, home=os.getcwd(), new_user=False, new_project=False):
+        '''
+        The User Management class manages the current users directories.
+        This class gives access to user paths, and provides functionality
+        for creating new projects for the current user within the users
+        home.
+        
+        :param repo (string):  The name of the repository.  
+        :param user (string):  The name of the current user if any.
+        :param project(string):  The name of the current project if any. 
+        :param home (string or pathlike):  The home path of the repository.
+        :param new_user (bool):  Flag for creating a new user. 
+        :param new_project (bool):  Flag for creating a new project. 
+        '''
         super().__init__(repo=repo, user=user, home=home, new_user=new_user)
         self.user = user
 
@@ -328,10 +299,29 @@ class UserMana(RepoMana):
     def zip_data(self):
         print('zip the users data and send it to their email')
 
+# datasnakes~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# datasnakes~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
 
 class WebMana(RepoMana):
 
     def __init__(self, repo, website, host='0.0.0.0', port='5252', home=os.getcwd(), new_website=False, create_admin=False):
+        '''
+        This installs a template for Flask using cookiecutter.  The 
+        custom datasnakes cookie for this template has been edited for
+        our own purposes.
+        
+        :param repo (string):  The name of the repository.  
+        :param website (string):  The name of the website.  Not a url, so
+        it doesn't containt http://www.*.com.  
+        (e.g. for www.vallenger-genetics.ml this parameter would be 'vallender-genetics')
+        :param host (string):  The address to launch the flask app.  Defaults to 0.0.0.0
+        :param port (string):  The port to launch the flask app.  Defaults to 5252  
+        :param home (string or pathlike):  The home path of the repository.
+        :param new_website (bool):  Flag for creating a new website 
+        :param create_admin:  Flag for creating a new admin for the website via FLASK USER.
+        (Note:  This parameter is not used currently in development.)
+        '''
         super().__init__(repo=repo, home=home)
         self.website = website
         self.web_host = host
@@ -346,6 +336,16 @@ class WebMana(RepoMana):
             self.create_website()
 
     def create_website(self):
+        '''
+        To create a website, the new_website cookie is used.
+        After creating the directory structure, the run_script function 
+        from cookiecutter finds the hooks folder which contains a
+        post-cookiecutter-template-generation bash script.  The bash script
+        sets up the proper dependencies and environment variables for the website,
+        and runs the website on the specified host and port
+        
+        :return: Runs the website.
+        '''
         # TODO-ROB Add heavy logging here
         e_c = {"website_name": self.website,
                "website_path": os.path.join(str(self.website_path), ''),
@@ -355,24 +355,28 @@ class WebMana(RepoMana):
         # Get the absolute path to the script that starts the flask server
         script_path = self.website_path / Path('hooks') / Path('post_gen_project.sh')
         #scripts_file_path = find_hook('post_gen_project.sh', hooks_dir=str(script_path))
+        # TODO-ROB add screening to the bash script for flask run -h -p
         run_script(script_path=str(script_path), cwd=str(self.website_path))
 
 
+# datasnakes~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# datasnakes~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 class ProjMana(UserMana):
 
     def __init__(self, repo, user, project, research=None, research_type=None, app=None, home=os.getcwd(),
                  new_project=False, new_research=False, new_app=False):
         """
-        :param repo: 
-        :param user: 
-        :param project: 
-        :param research: 
-        :param app: 
-        :param home: 
-        :param new_project: 
-        :param project_type: 
-        :param new_research: 
-        :param new_app: 
+        :param repo (string):  The name of the repository.  
+        :param user (string):  The name of the current user if any.
+        :param project(string):  The name of the current project if any.
+        :param research (string):  The name of the current type of research if any 
+        :param research_type (string):  The type of research (public or private)
+        :param app (string):  The name of the application that the research. 
+        :param home (string or pathlike):  The home path of the repository.
+        :param new_project (bool):  Flag for creating a new project. 
+        :param new_research (bool):  Flag for creating new research under a project.
+        :param new_app (bool):  Flag for creating a new web app under a research target. 
+
         """
         super().__init__(repo=repo, user=user, project=project, home=home,
                          new_project=new_project)
@@ -395,6 +399,12 @@ class ProjMana(UserMana):
             self.create_research(new_app)
 
     def create_research(self, new_app=False):
+        '''
+        :param new_app (bool):  Flag for auto generating an app that
+         goes with the research target.
+        :return:  Adds new directories in the current project labeled
+        with the proper names.
+        '''
         e_c = {"research_type": self.research_type,
                "research_name": self.research}
         cookiecutter(self.research_cookie, no_input=True, extra_context=e_c, output_dir=self.research_path)
