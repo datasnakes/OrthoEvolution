2f65deadfd26ad75627c2606d51d82d507d358b1
Robert Alan Gilmore <rgilmore@vallender-genetics.ml>
2017-05-12 02:34:23 +0000

    Added a test file.  There is no output for now.

---
+++ tester.py
@@ -0,0 +1,5 @@
+from Manager.utils.mana import Mana
+from Manager.utils.web_mana import WebMana
+x = Mana(repo='Tester', new_repo=True)
+y = WebMana(repo='Tester', website='Vall', new_website=True)
+
