396b076d61a809514bfb86957caa2d9d8d0f15a0
sdhutchins <sdhutchins@users.noreply.github.com>
2017-05-22 18:38:08 -0500

    Commented out PhyML test.

--- Orthologs/Phylogenetics/PhyML/__init__.py
+++ Orthologs/Phylogenetics/PhyML/__init__.py
@@ -1,10 +1,10 @@
 """PhyML tools."""
 
 from .orthophyml import PhyML
-from phyml_test.phyml_test import PhymlTest
+#from phyml_test.phyml_test import PhymlTest
 
 # Make this explicit, then they show up in the API docs
 __all__ = ("PhyML",
-           "PhymlTest",
+#           "PhymlTest",
 )
 
