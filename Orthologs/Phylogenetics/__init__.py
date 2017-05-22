fbfb87a7ce9128fd707c386332a02943ff290845
sdhutchins <sdhutchins@users.noreply.github.com>
2017-05-22 16:56:45 -0500

    Updated Phylogenetics init.py

--- Orthologs/Phylogenetics/__init__.py
+++ Orthologs/Phylogenetics/__init__.py
@@ -1,15 +1,23 @@
 """Phylogenetic Tools part of the Orthologs Package"""
 
-from PAML.__init__ import ETE3PAML, PamlTest
-from PhyML.__init__ import PhyML, PhymlTest
-from Phylip.__init__ import Phylip
-from PhyloTree.__init__ import TreeViz
+import warnings
+from Orthologs import OrthologsWarning
 
-# Make this explicit, then they show up in the API docs
-__all__ = ("ETE3PAML",
-           "PamlTest",
-           "PhyML",
-           "PhymlTest",
-           "TreeViz",
-           "Phylip",
-)
+# Ignore the warning in this init script.
+warnings.simplefilter('ignore', OrthologsWarning)
+
+#from PAML.__init__ import ETE3PAML, PamlTest
+#from PhyML.__init__ import PhyML, PhymlTest
+#from Phylip.__init__ import Phylip
+#from PhyloTree.__init__ import TreeViz
+#
+#
+#
+## Make this explicit, then they show up in the API docs
+#__all__ = ("ETE3PAML",
+#           "PamlTest",
+#           "PhyML",
+#           "PhymlTest",
+#           "TreeViz",
+#           "Phylip",
+#)
