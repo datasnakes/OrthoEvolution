0ff587fec5c2718e0800b133ac5b8a0951527bf0
rgilmore <rgilmore@umc.edu>
2017-05-22 15:11:04 -0500

    Cleanud up gi-list mp script.

--- Manager/index/get_gi_lists.py
+++ Manager/index/get_gi_lists.py
@@ -16,8 +16,8 @@ import pandas as pd
 from time import time
 from multiprocessing import Pool
 import logging as log
-from pathlib import Path
 
+# Set up logging
 log.basicConfig(filename="get_gi_lists.log", level=log.INFO)
 log.info("#------------------------------------------------------------------")
 log.info("The script name is %s" % os.path.basename(__file__))
@@ -25,40 +25,27 @@ log.info("The date and time is currently %s" % str(d.now()))
 log.info("#------------------------------------------------------------------")
 log.info("Create a GI list for each organism using the taxonomy id and the blastdbcmd tool on the MCSR.")
 
-#------------------------------------------------------------------------------
-# Open taxids.csv which is a comma delimited list of all tax id's to be used.
+# Get a taxid list from the file and change to the output directory
 tax_ids = pd.read_csv('taxids.csv', header=None, dtype=str)  # 1st column = tax id's
 tax_ids = list(tax_ids[0])
+os.chdir('data')
 
-#------------------------------------------------------------------------------
-# Create directory for Gi lists
-b = Path(os.getcwd()) / Path('data')  # Set a variable for the GiLists directory
-Path.mkdir(b, parents=True, exist_ok=True)  # Create the directory
-os.chdir(str(b))
 
-#------------------------------------------------------------------------------
-# Define the functions we want to use.
-def get_gilists(ID):
+def get_gilists(id):
     """ This function uses the blastdbcmd tool to get gi lists. It then uses the
     blastdb_aliastool to turn the list into a binary file.
-
-    The input (ID) for the function is a taxonomy ID.
+    The input (id) for the function is a taxonomy id.
     """
-    # Use the accession #'s and the blastdbcmd tool to generate gi lists based on Organisms/Taxonomy ID's.
-    os.system("blastdbcmd -db refseq_rna -entry all -outfmt '%g %T' | awk ' { if ($2 == " + ID + ") { print $1 } } ' > " + ID + "gi.txt")
-    log.info(ID + "gi.txt has been created.")
-
+    # Use the accession #'s and the blastdbcmd tool to generate gi lists based on Organisms/Taxonomy id's.
+    os.system("blastdbcmd -db refseq_rna -entry all -outfmt '%g %T' | awk ' { if ($2 == " + id + ") { print $1 } } ' > " + id + "gi.txt")
+    log.info(id + "gi.txt has been created.")
     # Convert the .txt file to a binary file using the blastdb_aliastool.
-    os.system("blastdb_aliastool -gi_file_in " + ID + "gi.txt -gi_file_out " + ID + "gi")
-    log.info(ID + "gi binary file has been created.")
-
+    os.system("blastdb_aliastool -gi_file_in " + id + "gi.txt -gi_file_out " + id + "gi")
+    log.info(id + "gi binary file has been created.")
     # Remove the gi.text file
-    os.system("rm -r " + ID + "gi.txt")
-    log.info(ID + "gi.text file has been deleted.")
+    os.system("rm " + id + "gi.txt")
+    log.info(id + "gi.text file has been deleted.")
 
-    # Move the gi file to a folder
-    os.system("mv " + ID + "gi data/gi-lists/")
-    log.info(ID + "gi binary file has been moved.")
 
 def main(idlist):
     """This function uses a pool to start multiple processes to get gi lists.
@@ -68,6 +55,6 @@ def main(idlist):
     with Pool(processes=10) as p:
         p.map(get_gilists, idlist)
         log.info("Took {} minutes to get all gi lists.".format((time() - ts)/60))
-#------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
 # Run the main function that creates the lists in parallel.
 main(idlist=tax_ids)
