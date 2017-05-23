3fdd1d77849281afac3eaec72e2a413ea4b2113a
sdhutchins <sdhutchins@users.noreply.github.com>
2017-05-23 12:01:20 -0500

    Added base testing classes and functions and added RelaxPhylip class to Phylogenetics init.

--- Orthologs/Phylogenetics/__init__.py
+++ Orthologs/Phylogenetics/__init__.py
@@ -1,15 +1,27 @@
-"""Phylogenetic Tools part of the Orthologs Package"""
+"""Phylogenetic tools part of the Orthologs Package"""
 
 import warnings
 from Orthologs import OrthologsWarning
+from Bio import AlignIO
 
 # Ignore the warning in this init script.
 warnings.simplefilter('ignore', OrthologsWarning)
 
+# Initialize the modules
 from Orthologs.Phylogenetics.PAML import ETE3PAML, PamlTest
 from Orthologs.Phylogenetics.PhyML import PhyML
 from Orthologs.Phylogenetics.Phylip import Phylip
 from Orthologs.Phylogenetics.PhyloTree import TreeViz
+
+# Add a new module
+class RelaxPhylip(object):
+    """Convert the a multiple sequence alignment file to
+    relaxed-phylip format.
+    """
+    def __init__(inputfile, outputfile):
+        """Fasta to Relaxed Phylip format."""
+        AlignIO.convert(inputfile, "fasta",
+                        outputfile, "phylip-relaxed")
 
 # Make this explicit, then they show up in the API docs
 __all__ = ("ETE3PAML",
@@ -17,4 +29,5 @@ __all__ = ("ETE3PAML",
            "PhyML",
            "TreeViz",
            "Phylip",
+           "RelaxPhylip",
 )
--- Tests/test_orthologs.py
+++ Tests/test_orthologs.py
@@ -1,14 +1,24 @@
-# -*- coding: utf-8 -*-
 """
 This is the test suite for the Orthologs Package.
 """
-from unittest import TestCase
-from Orthologs.Phylogenetics import PhyloTree
-from Orthologs import Align
+import unittest
+from Orthologs import OrthologsDevelopmentWarning
 
-# Under Development
-class Test(TestCase):
-    def test(self):
-        help(PhyloTree)
-        help(Align)
+class TestOrthologs(unittest.TestCase):
+    """Test the Orthologs Package."""
+    def test_phylo():
+        """Test the Phylogenetics modules."""
+    def test_align():
+        """Test the Align module."""
+    def test_compgenetics():
+        """Test the Comparative Genetics module."""
+    def test_genbank():
+        """Test the Genbank module."""
+    def test_blast():
+        """Test the Blast module."""
+    def test_biosql():
+        """Test the BioSQL module."""
 
+
+class TestManager(unittest.TestCase):
+    """Test the Manager Package."""
