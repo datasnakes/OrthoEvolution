e20c6c2fa7274ee455fd8940e9e1c5e558ebd5d5
sdhutchins <sdhutchins@users.noreply.github.com>
2017-05-23 15:54:29 -0500

    Commented out test suite as it gets developed.

--- Tests/test_orthologs.py
+++ Tests/test_orthologs.py
@@ -1,27 +1,27 @@
 """
 This is the test suite.
 """
-import unittest
+#import unittest
 from Orthologs import Phylogenetics as Phylo
 #from Orthologs import OrthologsDevelopmentWarning
 
-
-class TestOrthologs(unittest.TestCase):
-    """Test the Orthologs Package."""
-    def test_phylo():
-        """Test the Phylogenetics modules."""
-        help(Phylo.ETE3PAML)
-#    def test_align():
-#        """Test the Align module."""
-#    def test_compgenetics():
-#        """Test the Comparative Genetics module."""
-#    def test_genbank():
-#        """Test the Genbank module."""
-#    def test_blast():
-#        """Test the Blast module."""
-#    def test_biosql():
-#        """Test the BioSQL module."""
-#
-#
-# class TestManager(unittest.TestCase):
-#    """Test the Manager Package."""
+help(Phylo)
+#class TestOrthologs(unittest.TestCase):
+#    """Test the Orthologs Package."""
+#    def test_phylo(self):
+#        """Test the Phylogenetics modules."""
+#        help(Phylo.ETE3PAML)
+##    def test_align():
+##        """Test the Align module."""
+##    def test_compgenetics():
+##        """Test the Comparative Genetics module."""
+##    def test_genbank():
+##        """Test the Genbank module."""
+##    def test_blast():
+##        """Test the Blast module."""
+##    def test_biosql():
+##        """Test the BioSQL module."""
+##
+##
+## class TestManager(unittest.TestCase):
+##    """Test the Manager Package."""
