ac455b6dd4dc772d1e5c2bdc027e6a42f242cd56
sdhutchins <sdhutchins@users.noreply.github.com>
2017-05-23 12:00:47 -0500

    Fixed error in PhyML class.

--- Orthologs/Phylogenetics/PhyML/orthophyml.py
+++ Orthologs/Phylogenetics/PhyML/orthophyml.py
@@ -1,4 +1,3 @@
-from Bio import AlignIO
 from Bio.Phylo.Applications import PhymlCommandline
 import sys
 
@@ -8,32 +7,23 @@ class PhyML(object):
     from the PhyML executable. This class also converts a fasta formatted
     multiple sequence alignment file into relaxed phylip format.
     """
-    def __init__(self):
-        # Use the phyml executable file
-        phyml_exe = None
-
-        # This is mainly intended for windows use or use with an executable file
-        exe_name = "PhyML-3.1_win32.exe" if sys.platform == "win32" else "phyml"
-        phyml_exe = exe_name
-        self.phyml_exe = phyml_exe
-
-
-    def relaxphylip(self, inputfile, outputfile):
-        """Convert the file to relaxed-phylip format."""
-        AlignIO.convert(inputfile, "fasta",
-                        outputfile, "phylip-relaxed")
-
-    def runphyml(self, phyml_input):
+    def __init__(self, phyml_input, datatype='nt'):
         """Run phyml to generate tree results.
 
         If you're using Linux, ensure that your phyml path is set in your bash
         profile. If you're using Windows, this function will look for the name
         of the executable 'PhyML-3.1_win32.exe'.
         """
+        # Use the phyml executable file
+        phyml_exe = None
+
+        # This is mainly intended for windows use or use with an executable file
+        exe_name = "PhyML-3.1_win32.exe" if sys.platform == "win32" else "phyml"
+        phyml_exe = exe_name
+        phyml_exe = phyml_exe
 
         # Create the command & run phyml
         # Input a phylip formatted alignment file and describe the datatype ('nt' or 'aa')
-        run_phyml = PhymlCommandline(phyml_exe=self.phyml_exe, input=phyml_input, datatype='nt')
+        run_phyml = PhymlCommandline(phyml_exe, input=phyml_input, datatype=datatype)
         print(run_phyml)
         out_log, err_log = run_phyml()
-
