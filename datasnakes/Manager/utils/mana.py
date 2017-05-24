cfdfee30756c6cb7ea7f0ce8545bc31e93a08e5e
rgilmore <rgilmore@umc.edu>
2017-05-24 11:36:30 -0500

    Fixed _path attribute bug.

--- datasnakes/Manager/utils/mana.py
+++ datasnakes/Manager/utils/mana.py
@@ -44,7 +44,7 @@ class Mana(object):
         # TODO-ROB:  SOme of these directories don't need to be accessed directly
         # Below are the PyPi path strings
         #    The first group is to access the cookiecutter templates
-        self.Cookies = Path(Cookies.__path__._path[0])
+        self.Cookies = Path(Cookies.__path__[0])
         self.repo_cookie = self.Cookies / Path('new_repository')
         self.user_cookie = self.Cookies / Path('new_user')
         self.project_cookie = self.Cookies / Path('new_project')
@@ -52,13 +52,13 @@ class Mana(object):
         self.app_cookie = self.Cookies / Path('new_app')
         self.website_cookie = self.Cookies / Path('new_website')
         #    The second group is for the Manager module
-        self.Manager = Path(Manager.__path__._path[0])
+        self.Manager = Path(Manager.__path__[0])
         self.index = self.Manager / Path('index')
         self.logit = self.Manager / Path('logit')
         self.utils = self.Manager / Path('utils')
         self.shiny = self.Manager / Path('shiny')
         #    The third group is for the Orthologs module
-        self.Orthologs = Path(Orthologs.__path__._path[0])
+        self.Orthologs = Path(Orthologs.__path__[0])
         self.biosql = Path(self.Orthologs) / Path('biosql')
         self.blast = Path(self.Orthologs) / Path('blast')
         self.comp_gen = Path(self.Orthologs) / Path('comparative_genetics')
@@ -66,7 +66,7 @@ class Mana(object):
         self.manager = Path(self.Orthologs) / Path('manager')
         self.phylogenetics = Path(self.Orthologs) / Path('phylogenetics')
         #    The fourth group is for the Tools module
-        self.Tools = Path(Tools.__path__._path[0])
+        self.Tools = Path(Tools.__path__[0])
         self.ftp = Path(self.Tools) / Path('ftp')
         self.multiprocessing = Path(self.Tools) / Path('multiprocessing')
         self.pandoc = Path(self.Tools) / Path('pandoc')
