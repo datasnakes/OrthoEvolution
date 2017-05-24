98eee129415d671e728aa57f4ce35a0365bb0a79
rgilmore <rgilmore@umc.edu>
2017-05-24 17:29:21 -0500

    Update blasting_test

--- blasting_test.py
+++ blasting_test.py
@@ -21,20 +21,20 @@ project = "BLASTest"
 research = "comparative_genetics"
 research_type = "public"
 # Directory setup now combined with blast setup
-a = mana.ProjMana(repo=repo, user=user, project=project, research=research, research_type=research_type, new_project=True, new_research=True, new_repo=True, new_user=True)
-# a = mana.ProjMana(
-#     repo=repo,
-#     user=user,
-#     project=project,
-#     research=research,
-#     research_type=research_type)
+#a = mana.ProjMana(repo=repo, user=user, project=project, research=research, research_type=research_type, new_project=True, new_research=True, new_repo=True, new_user=True)
+a = mana.ProjMana(
+    repo=repo,
+    user=user,
+    project=project,
+    research=research,
+    research_type=research_type)
 shutil.copy(str(a.index / Path('MAFV3.2.csv')), str(a.project_index))
 Path.mkdir(a.raw_data / Path('blast') / Path('gi_lists'), parents=True)
 shutil.copy(str(a.index / Path('get_gi_lists.sh')),
             str(a.raw_data / Path('blast') / Path('gi_lists')))
 shutil.copy(str(a.index / Path('get_gi_lists.py')),
             str(a.raw_data / Path('blast') / Path('gi_lists')))
-#x = BLASTn(repo, user, project, research, research_type, template="MAFV3.2.csv", new_project=True, new_research=True, new_repo=True, new_user=True)
+# x = BLASTn(repo, user, project, research, research_type, template="MAFV3.2.csv", new_project=True, new_research=True, new_repo=True, new_user=True)
 x = BLASTn(
     repo=repo,
     user=user,
