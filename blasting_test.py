fbfbc5e0c013b41b3fba3fc8b53ab29a43c9e9ec
sdhutchins <sdhutchins@users.noreply.github.com>
2017-05-26 16:41:25 -0500

    Test a fix for numpy/scipy installations via travis ci

--- blasting_test.py
+++ blasting_test.py
@@ -1,28 +1,20 @@
-
-
-"""
-This is a test for the blastn function.
-"""
-import shutil
-from pathlib import Path
-
-from datasnakes.Manager.utils import mana
-
-# Modules used
-from datasnakes.Orthologs.Blast.blastn import BLASTn
+"""BLASTn test"""
+#import shutil
+#from pathlib import Path
+from datasnakes.Manager import ProjMana
+from datasnakes.Orthologs.Blast import BLASTn
 
 #from ete3 import NCBITaxa
 # NCBITaxa().update_taxonomy_database()
 
 # Initializations
 repo = "Test2"
-user = "rgilmore"
+user = "johndoe"
 project = "BLASTest"
 research = "comparative_genetics"
 research_type = "public"
 # Directory setup now combined with blast setup
-#a = mana.ProjMana(repo=repo, user=user, project=project, research=research, research_type=research_type, new_project=True, new_research=True, new_repo=True, new_user=True)
-a = mana.ProjMana(
+a = ProjMana(
     repo=repo,
     user=user,
     project=project,
@@ -34,7 +26,6 @@ a = mana.ProjMana(
 #             str(a.raw_data / Path('blast') / Path('gi_lists')))
 # shutil.copy(str(a.index / Path('get_gi_lists.py')),
 #             str(a.raw_data / Path('blast') / Path('gi_lists')))
-# x = BLASTn(repo, user, project, research, research_type, template="MAFV3.2.csv", new_project=True, new_research=True, new_repo=True, new_user=True)
 x = BLASTn(
     repo=repo,
     user=user,
