d8e8e63fd757e1e6af48b8008ecfcce8e8f2b756
sdhutchins <sdhutchins@users.noreply.github.com>
2017-05-26 13:26:25 -0500

    Updated flask_test.py to delete `Tester` directory.

--- flask_test.py
+++ flask_test.py
@@ -1,7 +1,11 @@
-"""
-Test for flask integration.
-"""
-# Modules Used
-from Manager.utils.mana import Mana, WebMana
+"""Test for flask integration."""
+import shutil
+import os
+from datasnakes.Manager.utils.mana import Mana, WebMana
 x = Mana(repo='Tester', new_repo=True)
 y = WebMana(repo='Tester', website='Vall', new_website=True)
+
+# Delete the created `Tester` directory
+if os.path.isdir('Tester') == True:
+    print('Tester directory exists. Now delete it.')
+    shutil.rmtree('Tester')
