9aa6aefc03c166f0a9dc550a5c880ea9cfc32636
sdhutchins <sdhutchins@users.noreply.github.com>
2017-05-26 12:02:43 -0500

    Updated __init__.py for Genbank module.

--- datasnakes/Orthologs/Genbank/__init__.py
+++ datasnakes/Orthologs/Genbank/__init__.py
@@ -1,8 +1,7 @@
 """Genbank tools."""
 
-from datasnakes.Orthologs.Genbank.archive.db2fasta import Database2Fasta as DB2FASTA
+from .genbank import Genbank
 
 # Make this explicit, then they show up in the API docs
-__all__ = ("DB2GBK",
-           "DB2FASTA",
+__all__ = ("Genbank",
            )
