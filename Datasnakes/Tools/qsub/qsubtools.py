7db622985656e6a2601cc6bef44d9c7b690401b3
sdhutchins <sdhutchins@users.noreply.github.com>
2017-05-26 12:44:46 -0500

    Made updates to qsubtools.py. Need to add the creation of a user's own pbs_dict.

--- Datasnakes/Tools/qsub/qsubtools.py
+++ Datasnakes/Tools/qsub/qsubtools.py
@@ -1,34 +1,21 @@
-# -*- coding: utf-8 -*-
+"""Collection of tools for using PBS, a job scheduler for high-performance
+computing environments. The command is usually `qsub <options>` on most systems.
 """
-File Name: SubmitQsubJob.py
-Description:
-
-Author: S. Hutchins
-Date Created: Thu May  4 16:18:08 2017
-Project Name: Orthologs Project
-"""
-
-# Modules Used
 import os
 import random
 import string
-# import tempfile  # This MAY be added later.
 import subprocess
 from string import Template
 from datetime import datetime as d
 import sys
 
-#------------------------------------------------------------------------------
-
-
 class CreateJob(object):
     def __init__():
-        """UNLESS A WINDOWS MACHINE HAS PBS (IT SHOULDNT)"""
+        """UNLESS A WINDOWS MACHINE HAS PBS (IT LIKELY WONT)"""
         if sys.platform == 'win32' or 'win64':
             sys.exit("This module is strictly for use on Linux at the moment.")
-#------------------------------------------------------------------------------
 
-    def ImportTemp(filepath):
+    def import_temp(filepath):
         """Import the script or file that you need a template of and that has temp
         strings."""
         file_temp = open(filepath, 'r')
@@ -38,31 +25,28 @@ class CreateJob(object):
         file_temp = Template(file_str)
         return file_temp
 
-#------------------------------------------------------------------------------
-    def File2Str(filename):
-        """ Turn the contents of a file (python file) into a string."""
+    def file2str(filename):
+        """Turn the contents of a file (python file) into a string."""
         file_temp = open(filename, 'r')
         file_str = file_temp.read()
         return file_str
 
-#------------------------------------------------------------------------------
-    def RandomId(self, length=5):
+    def randomid(self, length=5):
         """Generate a random ID of 5 characters to append to qsub job name."""
         return ''.join(random.sample(
             string.ascii_letters + string.digits, length))
 
-#------------------------------------------------------------------------------
-    def SubmitPythonCode(self, code, pbstemp, author,
+    def submitpythoncode(self, code, author, pbstemp='temp.pbs',
                          jobname="job", cleanup=True, prefix="", slots=1):
-        """ This function creates and submits qsub jobs."""
+        """Creates and submit a qsub jobs. Also use python code."""
 
         format1 = '%a %b %d %I:%M:%S %p %Y'  # Used to add as a date
 
         # Create a base name for the jobs and python scripts
-        base = prefix + "submit_{0}".format(self.RandomId())
+        base = prefix + "submit_{0}".format(self.randomid())
 
         # Create a python file and write the code to it
-        with open('tempfiles/' + base + '.py', 'w') as pyfile:
+        with open(base + '.py', 'w') as pyfile:
             pyfile.write(code)
             pyfile.close()
 
@@ -87,7 +71,7 @@ class CreateJob(object):
             'cmd': script
         }
 
-        # Create the pbs script from the template/dict
+        # Create the pbs script from the template or dict
         with open(base + '.pbs', 'w') as pbsfile:
             pbsfile.write(pbstemp.substitute(pbs_dict))
             pbsfile.close()
@@ -95,8 +79,7 @@ class CreateJob(object):
         # Submit the qsub job using subprocess
         try:
             cmd = 'qsub  ' + base + '.pbs'  # this is the command
-            cmd_status = subprocess.call(
-                [cmd], shell=True)  # Shell must be TRUE
+            cmd_status = subprocess.call([cmd], shell=True)  # Shell must be TRUE
             if cmd_status == 0:  # Command was successful.
                 pass  # Continue
             else:  # Unsuccessful. Stdout will be '1'.
@@ -106,10 +89,8 @@ class CreateJob(object):
                 os.remove(base + '.qsub')
                 os.remove(base + '.py')
 
-#------------------------------------------------------------------------------
     def MultipleJobs(self):
-        """Create multiple jobs & scripts for each job to run based on splitting
-        a list into chunks."""
+        """Create multiple jobs & scripts for each job to run based on splitting a list into chunks."""
 #        # Modules used
 #        from QsubTools import SubmitPythonCode, ImportTemp
 #        from multiprocess_functions import genes2align
