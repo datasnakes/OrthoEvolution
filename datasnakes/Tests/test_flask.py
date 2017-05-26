ae1d122f760ff6dc69db48682a337a4c9c724152
sdhutchins <sdhutchins@users.noreply.github.com>
2017-05-26 16:12:07 -0500

    Changed conditional statement for test_flask.py

--- datasnakes/Tests/test_flask.py
+++ datasnakes/Tests/test_flask.py
@@ -3,13 +3,16 @@ import shutil
 import os
 from datasnakes.Manager import Mana, WebMana
 
+
 def flask_test(directoryname='Test-Directory', website='TestSite'):
+    """"Test flask by creating a website/directory."""
     Mana(repo=directoryname, new_repo=True)
     WebMana(repo=directoryname, website=website, new_website=True)
+    # TODO-SDH Write a system exit for the flask tests.
 
     # Delete the created `Tester` directory
-    if os.path.isdir(directoryname) == True:
-        print('%s directory exists. Now delete it. Test passed.' % directoryname)
+    if os.path.isdir(directoryname) is True:
+        print('%s directory exists. Delete it. Test passed.' % directoryname)
         shutil.rmtree(directoryname)
     else:
-        print('%s directory DOES NOT exists. Debug failed test.' % directoryname)
+        print('%s directory DOES NOT exists. Test failed.' % directoryname)
