e3e8e569bc3d551fb8a846e8caca6e7fa7f85d3f
sdhutchins <sdhutchins@users.noreply.github.com>
2017-05-31 14:40:49 -0500

    Edit bash.py

--- Datasnakes/Tools/pybasher/bash.py
+++ Datasnakes/Tools/pybasher/bash.py
@@ -3,8 +3,23 @@
 # TODO-SDH Finish pybasher ASAP
 # TODO-SDH Look at some examples for pybasher.
 # TODO-SDH Update the README for pybasher
-from subprocess import PIPE, Popen
+import platform
 import sys
+
+
+SUBPROCESS_HAS_TIMEOUT = True
+if "windows" in platform.system().lower():
+    raise ImportError(
+            "sh %s is currently only supported on linux and osx.")
+elif sys.version_info < (3, 0):
+    try:
+        from subprocess32 import PIPE, Popen
+    except ImportError:
+        # You haven't got subprocess32 installed. If you're running 2.X this
+        # will mean you don't have access to things like timeout
+        SUBPROCESS_HAS_TIMEOUT = False
+
+from subprocess import PIPE, Popen, call
 #import os
 #import configparser
 # TODO-SDH use a config file to load/use a list or group of common commands.
@@ -12,22 +27,15 @@ import sys
 
 class PyBasher(object):
     # !!! Only for linux
-    def __init__(self, cmd):
+    def __init__(self, *args, **kwargs):
         """Initialize the call as well as standard error and output."""
         # TODO-SDH Test if this is working.
-        c = Popen([cmd], shell=True, stdout=PIPE, stderr=PIPE)
-        stdout, stderr = c.communicate()
-        if stderr:
-            sys.stderr
-        else:
-            sys.stdout          
-#        return c
-#
-#        __COMMANDS__ = [
-#                ]
-#        commands = __COMMANDS__
-#        return commands
-#    
-#    def listcommands(configfile='bash.cfg'):
-#        """Use a bash configuration file of common commands."""
-#        # IDEA not sure if this is the best option
+        self.process = None
+        self.stdout = None
+        self.pybasher(*args, **kwargs)
+        
+    def runcmd(self, cmd, env=None, stdout=PIPE, stderr=PIPE, timeout=None, sync=True):
+        self.process = Popen(cmd, shell=True, stdout=stdout, stdin=PIPE, stderr=stderr, env=env)
+        if sync:
+            self.sync(timeout)
+        return self
