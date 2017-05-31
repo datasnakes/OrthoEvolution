77220f1ba45234a65b0ca672b66752ccfadc63f6
sdhutchins <sdhutchins@outlook.com>
2017-05-31 18:17:52 -0500

    Qsub module is working. Need to work on MutliJob function and rename the main class.

--- Datasnakes/Tools/qsub/qsubtools.py
+++ Datasnakes/Tools/qsub/qsubtools.py
@@ -2,14 +2,15 @@
 computing environments. The command is usually `qsub <options>` on most
 systems.
 """
-import os
+import sys
 import random
 import string
 import subprocess
 from string import Template
 from datetime import datetime as d
-import sys
-
+import platform
+import getpass
+import os
 
 class CreateJob(object):
     """Create a pbs job and submit it using qsub.
@@ -17,13 +18,14 @@ class CreateJob(object):
     This class also provides functionality for creating multiple pbs jobs that
     by creating chunks of lists for each python script and job.
     """
-    def __init__():
-        """UNLESS A WINDOWS MACHINE HAS PBS IT LIKELY WONT"""
-        # XXX This class is optimized for Linux!
-        if sys.platform == 'win32' or 'win64':
-            sys.exit("This module is strictly for use on Linux at the moment.")
-
-    def import_temp(filepath):
+    def __init__(self):
+        """UNLESS A WINDOWS MACHINE HAS PBS. IT LIKELY WONT"""
+        if "windows" in platform.system().lower():
+            raise ImportError("PyBasher is currently only supported on linux and osx.")
+        
+        default = True # Default job is TRUE
+        self.default = default
+    def import_temp(self, filepath):
         """Import the script or file that you need a template of and that has
         temp strings.
         """
@@ -45,13 +47,24 @@ class CreateJob(object):
         return ''.join(random.sample(
             string.ascii_letters + string.digits, length))
 
-    def pbs_dict(self, author, email, description, project, select, jobname,
-                 script_name, logname, python='python3', gb='6gb',
-                 cput='75:00:00', walltime='75:00:00'):
+    def pbs_dict(self, default, author='', email='', description='', project='', select='1',
+                 jobname='', script_name='', logname='', python='python3',
+                 gb='6gb', cput='75:00:00', walltime='75:00:00'):
         """Add PBS script attributes."""
-        # Date format
-        format1 = '%a %b %d %I:%M:%S %p %Y'  # Used to add as a date
-        # Fillin the pbs script attributes
+        format1 = '%a %b %d %I:%M:%S %p %Y'  # Date format. Used to add as a date
+        
+        if self.default == default:
+            author = getpass.getuser().upper()
+            email = 'n/a'
+            description = 'This is a default pbs job.'
+            project = 'Datasnakes-Orthologs'
+            select = '3'
+            jobname = 'ds-ortho'
+            script_name = 'datasnakes'
+            logname = 'datasnakes'.format(self.randomid(length=3))
+        
+            
+        # Fill in the pbs script attributes
         job_attributes = {
             'author': author,
             'description': description,
@@ -65,15 +78,15 @@ class CreateJob(object):
             'wt': walltime,
             'job_name': jobname,
             'script': script_name,
-            'log_name': logname,
+            'log_name': logname.format(self.randomid()),
             'cmd': python + " " + script_name + ".py",
             'email': email
             }
 
         return job_attributes
 
-    def submitpythoncode(self, code, cleanup=True, prefix="", slots=1):
-        """Creates and submit a qsub job. Also use python code."""
+    def submitpythoncode(self, code, default=True, cleanup=True, prefix="", slots=1):
+        """Creates and submit a qsub job. Also uses python code."""
 
         # Create a base name for the jobs and python scripts
         base = prefix + "submit_{0}".format(self.randomid())
@@ -82,21 +95,30 @@ class CreateJob(object):
         with open(base + '.py', 'w') as pyfile:
             pyfile.write(code)
             pyfile.close()
-
-        # This is the "command" for running the python script.
         # TIP If python is in your environment as only 'python' update that.
         # TODO-SDH add a default parameter or custom parameter
-        # Custom will allow user to use whatever python script they want
         # If default, python file will be created from code that is used.
-        script = python + " " + base + ".py"
-
-        # Create the pbs script from the template or dict
-        with open(base + '.pbs', 'w') as pbsfile:
-            pbsfile.write(pbstemp.substitute(self.pbs_dict))
-            pbsfile.close()
-
-        # Submit the qsub job using subprocess
-        try:
+        if self.default == default:
+            # Create the pbs script from the template or dict
+            pbstemp = self.import_temp('temp.pbs')
+            with open(base + '.pbs', 'w') as pbsfile:
+                pbsfile.write(pbstemp.substitute(self.pbs_dict(self.default)))
+                pbsfile.close()
+        else:
+            pbstemp = self.import_temp('temp.pbs')
+            with open(base + '.pbs', 'w') as pbsfile:
+                pbsfile.write(pbstemp.substitute(self.pbs_dict(default=False)))
+                pbsfile.close()
+    
+            # Submit the qsub job using subprocess
+        user_input = input('Are you sure you want to submit this job? ')
+        if user_input != 'Yes' or 'yes' or 'Y' or 'si' or 'Si' or 'Ok':
+            # Delete the jobs that were just created if job not performed.
+            os.remove(base + '.pbs')
+            os.remove(base + '.py')
+            sys.exit('You do not want to submit this job.')  # Exit.
+            
+        try:                
             cmd = 'qsub  ' + base + '.pbs'  # this is the command
             print(cmd)
 #            cmd_status = subprocess.call([cmd], shell=True)  # must = TRUE
@@ -105,16 +127,17 @@ class CreateJob(object):
 #            else:  # Unsuccessful. Stdout will be '1'.
 #                print("PBS job not submitted.")
         finally:
-            print('finally')
+            print('hi')
 #            if cleanup:  # When finished, remove the qsub files & python files.
 #                os.remove(base + '.qsub')
 #                os.remove(base + '.py')
 
-    def multiplejobs(self):
-        """Create multiple jobs & scripts for each job to run based on
-        splitting a list into chunks.
-        """
-        print(self.__doc__)
+
+#    def multiplejobs(self):
+#        """Create multiple jobs & scripts for each job to run based on
+#        splitting a list into chunks.
+#        """
+#        print(self.__doc__)
 #        # Modules used
 #        from QsubTools import SubmitPythonCode, ImportTemp
 #        from multiprocess_functions import genes2align
