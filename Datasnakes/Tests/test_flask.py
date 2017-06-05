a401a27c59086069e2b68c4ed51545e2b65dc063
sdhutchins <sdhutchins@outlook.com>
2017-06-05 09:32:49 -0500

    More edits to flask test.

--- Datasnakes/Tests/test_flask.py
+++ Datasnakes/Tests/test_flask.py
@@ -11,13 +11,11 @@ def flask_test(directoryname='Flask-Test', website='TestSite'):
         WebMana(repo=directoryname, website=website, new_website=False)
         # new_website=False in order to prevent starting of server.
 
-        # Delete the created `Tester` directory
         if os.path.isdir(directoryname) is True:
             print('%s directory exists. Deleted it. Test passed.' % directoryname)
+            # Delete the created `Tester` directory
             shutil.rmtree(directoryname)
         else:
             print('%s directory was not created.' % directoryname)
-    except:
+    except:  # Raise an exception if the directories are not created.
         raise Exception('Error with Mana or WebMana.')
-
-flask_test()
