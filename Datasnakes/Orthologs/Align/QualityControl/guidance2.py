d13f60a2e866c7794de2d88d12413a5708f5045f
rgilmore <rgilmore@umc.edu>
2017-07-26 17:37:44 -0500

    Finished but untested command line wrapper for Guidance2.

--- Datasnakes/Orthologs/Align/QualityControl/guidance2.py
+++ Datasnakes/Orthologs/Align/QualityControl/guidance2.py
@@ -15,6 +15,7 @@ from __future__ import print_function
 from pathlib import Path
 from Bio.Application import _Option, _Switch, AbstractCommandline
 
+
 class Guidance2Commandline(AbstractCommandline):
     u""""Command line wrapper for GUIDANCE2.
     http://guidance.tau.ac.il/ver2/
@@ -44,7 +45,8 @@ class Guidance2Commandline(AbstractCommandline):
                 _Option(['--seqFile', 'seqFile'],
                         "Input sequence file in FASTA format",
                         filename=True, equate=False, is_required=True,
-                        checker_function=lambda x: Path(x).suffix in ['.fasta', 'fna', '.ffn', '.faa', '.fra']),
+                        checker_function=lambda x: Path(x).suffix in ['.fasta', 'fna', '.ffn', '.faa', '.fra']
+                        and Path(x).is_file()),
                 _Option(['--msaProgram', 'msaProgram'],
                         "Which MSA program to use",
                         equate=False, is_required=True,
@@ -65,8 +67,9 @@ class Guidance2Commandline(AbstractCommandline):
                         equate=False,
                         checker_function=lambda x: x in ["GUIDANCE2", "GUIDANCE", "HoT"]),
                 _Option(['--bootstraps', 'bootstraps'],
-                        "",
-                        equate=False),
+                        "Number of bootstrap iterations (only for GUIDQANCE). Defaut=100",
+                        equate=False,
+                        checker_function=lambda x: isinstance(x, int)),
                 _Option(['--genCode', 'genCode'],
                         "Genetic code identifier (only for codon sequences). Default=1 \
                             1) Nuclear Standard\
@@ -80,34 +83,57 @@ class Guidance2Commandline(AbstractCommandline):
                             9) Mitochondria Echinoderm\
                             14) Mitochondria Flatworm\
                             4) Mitochondria Protozoan",
-                        equate=False),
+                        equate=False,
+                        checker_function=lambda x: isinstance(x, int)),
                 _Option(['--outOrder', 'outOrder'],
-                        "",
-                        equate=False),
+                        "[aligned|as_input] default=aligned",
+                        equate=False,
+                        checker_function=lambda x: x in ['aligned', 'as_input']),
                 _Option(['--msaFile', 'msaFile'],
-                        "",
-                        equate=False),
-                _Option(['', ''],
-                        "",
-                        equate=False),
-                _Option(['', ''],
-                        "",
-                        equate=False),
-                _Option(['', ''],
-                        "",
-                        equate=False),
-                _Option(['', ''],
-                        "",
-                        equate=False),
-                _Option(['', ''],
-                        "",
-                        equate=False),
-                _Option(['', ''],
-                        "",
-                        equate=False),
-                _Option(['', ''],
-                        "",
+                        "Input alignment file - not recommended",
+                        filename=True, equate=False,
+                        checker_function=lambda x: Path(x).is_file()),
+                # Confidence scores
+                _Option(['--seqCutoff', 'seqCutoff'],
+                        "Confidence cutoff between 0 to 1. Default=0.6",
+                        equate=False,
+                        checker_function=lambda x: isinstance(x, int)),
+                _Option(['--colCutoff', 'colCutoff'],
+                        "Confidence cutoff between 0 to 1. Default=0.93",
                         equate=False),
 
-
+                # Alignment Programs
+                _Option(['--mafft', 'mafft'],
+                        "path to mafft executable. Default=mafft",
+                        filename=True, equate=False,
+                        checker_function=lambda x: Path(x).is_file()),
+                _Option(['--prank', 'prank'],
+                        "path to prank executable. Default=prank",
+                        filename=True, equate=False,
+                        checker_function=lambda x: Path(x).is_file()),
+                _Option(['--muscle', 'muscle'],
+                        "path to muscle executable. default=muscle",
+                        filename=True, equate=False,
+                        checker_function=lambda x: Path(x).is_file()),
+                _Option(['--pagan', 'pagan'],
+                        "path to pagan executable, default=pagan",
+                        filename=True, equate=False,
+                        checker_function=lambda x: Path(x).is_file()),
+                _Option(['--ruby', 'ruby'],
+                        "path to ruby executable. default=ruby",
+                        filename=True, equate=False,
+                        checker_function=lambda x: Path(x).is_file()),
+                # Miscellaneous
+                _Option(['--dataset', 'dataset'],
+                        "Unique name for the Dataset - will be used as prefix to outputs (default=MSA)",
+                        equate=False),
+                _Option(['--MSA_Param', 'MSA_Param'],
+                        "passing parameters for the alignment program e.g -F to prank. "
+                        "To pass parameter containning '-' in it, add \ before each '-' e.g. \-F for PRANK",
+                        equate=False),
+                _Option(['--proc_num', 'proc_num'],
+                        "number of processors to use (default=1)",
+                        equate=False,
+                        checker_function=lambda x: isinstance(x, int))
             ]
+        AbstractCommandline.__init(self, cmd, **kwargs)
