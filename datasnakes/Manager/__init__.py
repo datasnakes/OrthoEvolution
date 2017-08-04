83b7d616b0c595be628507ed04e7ccf5596ddbc9
rgilmore <rgilmore@umc.edu>
2017-08-03 19:14:33 -0500

    Updated imports.

--- datasnakes/Manager/__init__.py
+++ datasnakes/Manager/__init__.py
@@ -1,8 +1,8 @@
 """Package Management Tools"""
 
 # Initialize the modules
-from datasnakes.Manager.logit import LogIt
-from datasnakes.Manager.utils import WebMana, ProjMana, ZipUtils, Mana, RepoMana, UserMana
+from Datasnakes.Manager.logit import LogIt
+from Datasnakes.Manager.utils import WebMana, ProjMana, ZipUtils, Mana, RepoMana, UserMana
 
 # Make this explicit, then they show up in the API docs
 __all__ = ("WebMana",
