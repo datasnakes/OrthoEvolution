69f6569a4a9e3c9d0657f47e90613ccf319025b7
rgilmore <rgilmore@umc.edu>
2017-08-04 14:02:47 -0500

    Created the FilteredTree class which creates a tree using iqtree. It also handles the directories and files.

---
+++ Datasnakes/Orthologs/Phylogenetics/IQTree/best_tree.py
@@ -0,0 +1,24 @@
+import os
+from pathlib import Path
+from Datasnakes.Orthologs.Phylogenetics.IQTree.iqtree import IQTreeCommandline
+from shutil import copy
+#TODO-ROB Make this inherit FilteredAlignment
+
+class FilteredTree(object):
+
+    def __init__(self, alignment, dataType='CODON', home=os.getcwd()):
+        self.home = Path(home)
+        self.iqtree_path = self.home / Path('IQTREE')
+        self.gene = alignment.replace('_P2N_na.aln', '')
+
+        self.aln_File = str(self.home / Path(alignment))
+        outDir = self.home / Path('IQTREE')
+        Path.mkdir(outDir, exist_ok=True)
+        copy(self.aln_File, outDir)
+        os.chdir(outDir)
+        self.iqtree_best_tree(alignment, dataType)
+
+    def iqtree_best_tree(self, alignment, dataType):
+        IQTCmd = IQTreeCommandline(alignment=alignment, dataType=dataType)
+        print(IQTCmd)
+        IQTCmd()
