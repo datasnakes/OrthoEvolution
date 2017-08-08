37258571598f4897aa54df5c2cc336bd373d1674
rgilmore <rgilmore@umc.edu>
2017-08-08 14:57:45 -0500

    Added a sorting function to sort alignments based on reference file. Changed parameters from full_file to target file and from id_file to reference_file.

--- Datasnakes/Orthologs/Genbank/utils.py
+++ Datasnakes/Orthologs/Genbank/utils.py
@@ -1,43 +1,62 @@
 from Bio import SeqIO
+from Bio import AlignIO
+from Bio.Align import MultipleSeqAlignment
+from Bio.Alphabet import IUPAC, Gapped
 from pathlib import Path
 from tempfile import TemporaryFile
 import itertools
 
-def multi_fasta_manipulator(full_file, id_file, output, manipulation='remove'):
+def multi_fasta_manipulator(target_file, reference_file, output, manipulation='remove'):
     # Inspired by the BioPython Tutorial and Cookbook ("20.1.1 Filtering a sequence file")
     """
     This method manipulated selected sequences in a multi-FASTA files.  The original
     purpose was to filter files created by the GUIDANCE2 alignment program, but
     the function has been made in order to accommodate other situations as well.
 
-    :param full_file:  Target multi-FASTA file.
-    :param id_file:  Selected sequences for removal in a multi-FASTA file.
+    :param target_file:  Target multi-FASTA file.
+    :param reference_file:  Selected sequences for removal in a multi-FASTA file.
     :param manipulation:  Type of manipulation.  (remove, add, tbd..)
     :param added_name:  The output file uses this parameter to name itself.
     :return:  A multi-FASTA file with filter sequences.
     """
     # Create path variables
     file_name = output
-    new_file = Path(full_file).parent / Path(file_name)
-    # Turn the id_file into set of ids
-    ids = set(record.id for record in SeqIO.parse(id_file, 'fasta'))
-    # Create a new multi-fasta record object using the full_file and the newly generated set of ids
+    new_file = Path(target_file).parent / Path(file_name)
+    if reference_file is not None:
+        # Turn the reference_file into set of ids
+        ids = set(record.id for record in SeqIO.parse(reference_file, 'fasta'))
+    # Create a new multi-fasta record object using the target_file and the newly generated set of ids
     if manipulation is 'remove':
-        new_records = (record for record in SeqIO.parse(full_file, 'fasta') if record.id not in ids)
+        new_records = (record for record in SeqIO.parse(target_file, 'fasta') if record.id not in ids)
         print('Sequences have been filtered.')
         SeqIO.write(new_records, str(new_file), 'fasta')
     # Combine all the FASTA sequence in one record object
-    elif manipulation == 'add':
+    elif manipulation is 'add':
         # Concatenate the multifasta files together by chaining the SeqIO.parse generators
         # Allows one to overwrite a file by using temporary files for storage
         # adding generators - https://stackoverflow.com/questions/3211041/how-to-join-two-generators-in-python
-        with TemporaryFile('r+', dir=Path(full_file).parent) as tmp_file:
-            new_records = itertools.chain(SeqIO.parse(full_file, 'fasta',), SeqIO.parse(id_file, 'fasta'))
+        with TemporaryFile('r+', dir=Path(target_file).parent) as tmp_file:
+            new_records = itertools.chain(SeqIO.parse(target_file, 'fasta', ), SeqIO.parse(reference_file, 'fasta'))
             count = SeqIO.write(new_records, tmp_file, 'fasta')
             tmp_file.seek(0)
             print('temp file count: ' + str(count))
             SeqIO.write(SeqIO.parse(tmp_file, 'fasta'), str(new_file), 'fasta')
         print('Sequences have been added.')
+    elif manipulation is 'sort':
+        with TemporaryFile('r+', dir=Path(target_file).parent) as tmp_file:
+            aln = MultipleSeqAlignment([])
+            for sorted_seq_record in SeqIO.parse(reference_file, 'fasta'):
+                for unsorted_aln_record in SeqIO.parse(target_file, 'fasta'):
+                    if unsorted_aln_record.id == sorted_seq_record.id:
+                        print(unsorted_aln_record.id)
+                        print(sorted_seq_record.id)
+                        aln.append(unsorted_aln_record)
+                        break
+            count = AlignIO.write(aln, tmp_file, 'fasta')
+            tmp_file.seek(0)
+            print('temp file count: ' + str(count))
+            AlignIO.write(AlignIO.read(tmp_file, 'fasta'), str(new_file), 'fasta')
+        print('Alignment has been sorted.')
     print('A new fasta file has been created.')
     return new_file
 
