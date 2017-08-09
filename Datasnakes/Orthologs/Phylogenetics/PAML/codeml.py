a6592c121abe9b32b26b8912b18dd49491b2f79c
rgilmore <rgilmore@umc.edu>
2017-08-09 11:56:09 -0500

    Updated pathlib object to string.

--- Datasnakes/Orthologs/Phylogenetics/PAML/codeml.py
+++ Datasnakes/Orthologs/Phylogenetics/PAML/codeml.py
@@ -3,6 +3,7 @@ import os
 from pathlib import Path
 from shutil import copy
 
+
 class CodemlRun(object):
 
     def __init__(self, P2N_alignment, iqtree_newick, control_file, home=os.getcwd()):
@@ -10,16 +11,17 @@ class CodemlRun(object):
         self.paml_path = self.home / Path('PAML')
         self.paml_path.mkdir(exist_ok=True)
         self.gene = str(iqtree_newick).replace('_iqtree.nwk', '')
+        self.control_file = self.paml_path / Path(self.gene + '.ctl')
         copy(P2N_alignment, str(self.paml_path))
         copy(iqtree_newick, str(self.paml_path))
 
-        self.cml = codeml.Codeml(P2N_alignment, iqtree_newick, working_dir=self.paml_path, out_file=self.gene +'_codeml.out')
+        self.cml = codeml.Codeml(P2N_alignment, iqtree_newick, working_dir=str(self.paml_path), out_file=self.gene +'_codeml.out')
         self.control_setup(control_file)
 
     def control_setup(self, control_file):
         self.cml.read_ctl_file(control_file)
         self.cml.print_options()
-        self.cml.ctl_file = str(self.paml_path / Path(self.gene + '.ctl'))
+        self.cml.ctl_file = str(self.control_file)
         self.cml.write_ctl_file()
 
 
