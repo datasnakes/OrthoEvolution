3660b2bb4e37c3cd6d9e0b09f3aa62c0f9178aae
rgilmore <rgilmore@umc.edu>
2017-08-08 21:06:00 -0500

    Added a line to copy the treefile.

--- Datasnakes/Orthologs/Phylogenetics/IQTree/best_tree.py
+++ Datasnakes/Orthologs/Phylogenetics/IQTree/best_tree.py
@@ -5,21 +5,25 @@ from shutil import copy
 import subprocess
 #TODO-ROB Make this inherit FilteredAlignment
 
+
 class FilteredTree(object):
 
     def __init__(self, alignment, dataType='CODON', home=os.getcwd()):
         self.home = Path(home)
         self.iqtree_path = self.home / Path('IQTREE')
+        self.tree_file = self.iqtree_path / alignment + '.treefile'
         self.gene = alignment.replace('_P2N_na.aln', '')
 
         self.aln_File = str(self.home / Path(alignment))
         outDir = self.home / Path('IQTREE')
         Path.mkdir(outDir, exist_ok=True)
-        copy(self.aln_File, outDir)
-        os.chdir(outDir)
+        copy(self.aln_File, str(outDir))
+        os.chdir(str(outDir))
         self.iqtree_best_tree(alignment, dataType)
+        copy(self.tree_file, str(self.home / Path(self.gene + '_iqtree.nwk')))
 
     def iqtree_best_tree(self, alignment, dataType):
+
         IQTCmd = IQTreeCommandline(alignment=alignment, dataType=dataType)
         print(IQTCmd)
         subprocess.check_call([str(IQTCmd)], stderr=subprocess.STDOUT, shell=True)
