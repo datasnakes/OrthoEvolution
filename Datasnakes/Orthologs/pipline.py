187f6a1277ad311e79309e32568803f853c38f90
rgilmore <rgilmore@umc.edu>
2017-08-09 15:40:48 -0500

    Created a master class to run and submit jobs based on input. Each gene gets its own job.

--- Datasnakes/Orthologs/pipline.py
+++ Datasnakes/Orthologs/pipline.py
@@ -3,36 +3,35 @@ from Datasnakes.Orthologs.Phylogenetics.IQTree.best_tree import FilteredTree
 from Datasnakes.Orthologs.Phylogenetics.PAML.codeml import CodemlRun
 import os
 import time
+import subprocess
 from shutil import copy
 from pathlib import Path
 
 
 class OrthologPipeline(object):
 
-    def __init__(self, genes, qsub_template, python_file, home=os.getcwd()):
+    def __init__(self, genes, qsub_template, worker_template, home=os.getcwd()):
         self.index = Path(home)  # index directory
         self.raw_data = self.index.parent / Path('raw_data')
         self.data = self.index.parent / Path('data')
-        with open(genes, 'r') as gene_list:
+        self.qsub_template = self.index / qsub_template
+        self.worker_template = self.index / worker_template
+        self.genes = genes
+
+    def iterate(self):
+        with open(self.genes, 'r') as gene_list:
             for gene in gene_list.readlines():
-                qsub_file = copy(str(self.index / Path(qsub_template)),
-                                 str(self.raw_data / Path(gene) / Path(gene + '_job.sh')))
+                command = self.batch_script_setup(str(self.qsub_template), str(self.worker_template), str(self.raw_data / Path(gene)), 'rgilmore',
+                                        'rgilmore@gmail.com', '8GB', gene)
+                self.submit(command)
 
-                self.na_fasta = str(self.raw_data / Path(gene + '.ffn'))
-                self.aa_fasta = str(self.raw_data / Path(gene + '.faa'))
-                self.P2N_na_alignment = str(self.raw_data / Path(gene + '_P2N_na.aln'))
-                self.iqtree_newick = str(self.raw_data / Path(gene + 'iqtree.nwk'))
-                self.paml_control = str(self.raw_data / Path('paml.ctl'))
-
-
+    @staticmethod
     def batch_script_setup(self, qsub_file, python_file, raw_data_path, author, email, memory, gene):
         date_time = time.strftime("%H:%M:%S on %d/%m/%Y")
         qsub_command = "qsub -v PYTHONFILE=%s RAWDATA=%s AUTHOR=%s EMAIL=%s GBS=%s GENE=%s DATE=%s %s" % \
                        (python_file, raw_data_path, author, email, memory, gene, date_time, qsub_file)
-    # qca = FilteredAlignment('HTR1A.ffn', 'HTR1A.faa', na_colCutoff=0.1)
-    # qct = FilteredTree('HTR1A_P2N_na.aln')
-    #
-    # os.chdir('/work5/r2294/bin/Datasnakes/Orthologs/Align/QualityControl')
-    # x = CodemlRun('HTR1A_P2N_na.aln', 'HTR1A_iqtree.nwk', 'paml.ctl')
-    #
-    # x.cml.run(verbose=True)
+        return qsub_command
+
+    @staticmethod
+    def submit(self, qsub_command):
+        subprocess.check_call([qsub_command], stderr=subprocess.STDOUT, shell=True)
