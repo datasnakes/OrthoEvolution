5e4fbd31c4396c6f347f819bba5caab666b62e64
rgilmore <rgilmore@umc.edu>
2017-08-09 14:59:32 -0500

    Created a Template pipeline.sh file. Each gene gets its own job.

---
+++ Datasnakes/Orthologs/pipeline.sh
@@ -0,0 +1,20 @@
+# Author: ${AUTHOR}
+# Date Created: {$DATE}
+# Project Name: QC_test
+# Description: Testing the quality control of the new phylogenetics pipeline.
+
+#PBS -S /bin/bash
+#PBS -m bea
+#PBS -M ${EMAIL}
+#PBS -l mem=${GBS}
+#PBS -N ${AUTHOR}_${GENE}
+#PBS -o ${AUTHOR}_${GENE}.o$PBS_JOBID
+#PBS -e ${AUTHOR}_${GENE}.e$PBS_JOBID
+#PBS -j oe
+
+# change into ${RAWDATA}
+cd ${RAWDATA}
+
+python36 ${PYTHONFILE} arg1 arg2 arg3
+
+mail -s "${AUTHOR}_${GENE} script completed" ${EMAIL} <<< 'Check your output'
