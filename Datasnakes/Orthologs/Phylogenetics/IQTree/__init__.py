1bf59934364a0ced8c98cfad8d9f42a9ecedb04f
rgilmore <rgilmore@umc.edu>
2017-08-10 11:36:28 -0500

    Added __init__ file for IQ-Tree.

---
+++ Datasnakes/Orthologs/Phylogenetics/IQTree/__init__.py
@@ -0,0 +1,8 @@
+from .best_tree import FilteredTree
+from .iqtree import IQTreeCommandline
+
+
+
+# Make this explicit, then they show up in the API docs
+__all__ = ("FilteredTree", "IQTreeCommandline"
+           )
