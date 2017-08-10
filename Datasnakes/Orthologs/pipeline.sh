1a686a4dd6b9bad6124d8614dbdbdbaad53e3187
rgilmore <rgilmore@umc.edu>
2017-08-10 10:44:50 -0500

    Added added a line to load the .bashrc file for my aliases.

--- Datasnakes/Orthologs/pipeline.sh
+++ Datasnakes/Orthologs/pipeline.sh
@@ -1,20 +1,21 @@
-# Author: ${AUTHOR}
-# Date Created: {$DATE}
+# Author: $AUTHOR
+# Date Created: $DATE
 # Project Name: QC_test
 # Description: Testing the quality control of the new phylogenetics pipeline.
 
+. ~/.bashrc
 #PBS -S /bin/bash
 #PBS -m bea
-#PBS -M ${EMAIL}
-#PBS -l mem=${GBS}
-#PBS -N ${AUTHOR}_${GENE}
-#PBS -o ${AUTHOR}_${GENE}.o$PBS_JOBID
-#PBS -e ${AUTHOR}_${GENE}.e$PBS_JOBID
+#PBS -M $EMAIL
+#PBS -l mem=$GBS
+#PBS -N $AUTHOR_$GENE
+#PBS -o $AUTHOR_$GENE.o$PBS_JOBID
+#PBS -e $AUTHOR_$GENE.e$PBS_JOBID
 #PBS -j oe
 
-# change into ${RAWDATA}
-cd ${RAWDATA}
+# change into $RAWDATA
+cd $RAWDATA
 
-python36 ${PYTHONFILE} ${GENE} ${RAWDATA}
+python36 $PYTHONFILE $GENE $RAWDATA
 
-mail -s "${AUTHOR}_${GENE} script completed" ${EMAIL} <<< 'Check your output'
+mail -s "$AUTHOR_$GENE script completed" $EMAIL <<< 'Check your output'
