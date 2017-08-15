117ebe397f3610b70b77af8bbc29620edc05c7b4
rgilmore <rgilmore@umc.edu>
2017-08-15 16:22:34 -0500

    Added package resources for development.

--- Datasnakes/Orthologs/CompGenetics/ncbi_blast.py
+++ Datasnakes/Orthologs/CompGenetics/ncbi_blast.py
@@ -4,7 +4,9 @@ from pathlib import Path
 import pandas as pd
 from Datasnakes.Orthologs.CompGenetics import CompGenAnalysis
 from Datasnakes.Manager.logit import LogIt
-
+import pkg_resources
+import shutil
+from Datasnakes.Manager import index
 
 class BLASTAnalysis(CompGenAnalysis):
     """Perform Blast Analysis after completing BLASTn."""
@@ -27,6 +29,8 @@ class BLASTAnalysis(CompGenAnalysis):
         self.save_data = save_data
 
         if template is not None:
+            if kwargs['copy_from_package']:
+                shutil.copy(pkg_resources.resource_filename(index.__name__, kwargs['MAF']))
             self.template_filename = template
             self.template_path = self.project_index / \
                 Path(self.template_filename)
