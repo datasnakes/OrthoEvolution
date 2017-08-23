7c6a5abf029b08966b7cfdc01f065da6e3d5053c
sdhutchins <sdhutchins@outlook.com>
2017-08-23 13:37:03 -0500

    Update to requirements.txt

--- Datasnakes/Orthologs/sudo-install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# For Debian/Ubuntu users
-sudo apt-get update; sudo apt-get upgrade
-
-# Install the packages
-sudo apt-get install clustalo paml phyml ncbi-blast+ phylip
-echo "All packages installed."
-
-# Set the PATHs
