09a6a8216eb4a7374518ee14a4ba33ebacd51118
sdhutchins <sdhutchins@users.noreply.github.com>
2017-08-28 17:48:36 -0500

    updated genbank_mana.py

--- Datasnakes/Orthologs/Genbank/genbank_mana.py
+++ Datasnakes/Orthologs/Genbank/genbank_mana.py
@@ -1,12 +1,10 @@
 #import os
 #from pathlib import Path
 from .genbank import GenBank as GB
-from Datasnakes.Tools.ftp.ftp2db import FTP
 
-class GenBankMana(FTP, GB):
+class GenBankMana(GB):
     """
     """
     def __init__(self):
-        FTP.__init__(self)
         GB.__init__(self)
         print("I don't do anything but look cool.")
