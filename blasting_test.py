d34681b1af5264c2a7aa5bc99e3cb8186cb29f61
rgilmore <rgilmore@umc.edu>
2017-08-28 11:13:29 -0500

    Update.

--- blasting_test.py
+++ blasting_test.py
@@ -1,28 +1,28 @@
 """BLASTn test"""
-#import shutil
+import shutil
 #from pathlib import Path
-from Datasnakes.Manager import ProjMana
+from Datasnakes.Manager.utils.mana import ProjMana
+from Datasnakes.Manager import index
 from Datasnakes.Orthologs.Blast import BLASTn
-
-#from ete3 import NCBITaxa
+# from ete3 import NCBITaxa
 # NCBITaxa().update_taxonomy_database()
-
+import pkg_resources
 # Initializations
 repo = "Test2"
 user = "johndoe"
 project = "BLASTest"
 research = "comparative_genetics"
 research_type = "public"
-
+template = pkg_resources.resource_filename(index.__name__, "MAFV3.2.csv")
 # Directory setup now combined with blast setup
-a = ProjMana(
-    repo=repo,
-    user=user,
-    project=project,
-    research=research,
-    research_type=research_type)
+# a = ProjMana(
+#     repo=repo,
+#     user=user,
+#     project=project,
+#     research=research,
+#     research_type=research_type, new_repo=True)
 
-# shutil.copy(str(a.index / Path('MAFV3.2.csv')), str(a.project_index))
+
 # Path.mkdir(a.raw_data / Path('blast') / Path('gi_lists'), parents=True)
 # shutil.copy(str(a.index / Path('get_gi_lists.sh')),
 #             str(a.raw_data / Path('blast') / Path('gi_lists')))
@@ -34,7 +34,7 @@ x = BLASTn(
     project=project,
     research=research,
     research_type=research_type,
-    template="MAFV3.2.csv")
+    template=template, new_repo=True, new_project=True, new_research=True, new_user=True, copy_from_package=True, MAF='MAFV3.2.csv')
 BLASTER = x.blast_config
 BLASTER(x.blast_human, 'Homo_sapiens', auto_start=True)
 # x.post_blast_analysis("Vall")
