4e7240e32f2bec8dcce2a0240a3c13ac8fde8bdf
sdhutchins <sdhutchins@users.noreply.github.com>
2017-08-28 19:25:32 -0500

    Added pipeline test format.

--- Datasnakes/Orthologs/Genbank/genbank_mana.py
+++ Datasnakes/Orthologs/Genbank/genbank_mana.py
@@ -1,10 +1,7 @@
-#import os
-#from pathlib import Path
 from .genbank import GenBank as GB
 
 class GenBankMana(GB):
-    """
-    """
+    """Genbank file management."""
     def __init__(self):
         GB.__init__(self)
         print("I don't do anything but look cool.")
--- Datasnakes/Pipeline/test.py
+++ Datasnakes/Pipeline/test.py
@@ -1,16 +1,37 @@
-"""Simple test of one gene through the entire pipeline"""
-
+"""Simple test of one gene through the entire pipeline."""
+# import os
+# import sys
+# import zipfile
 import luigi
+import logzero as log
+from pathlib import Path
+from datetime import datetime as d
+
+from Datasnakes.Manager.utils.mana import ProjMana  # Project Management
+from Datasnakes.Orthologs.Blast.blastn import BLASTn
+from Datasnakes.Orthologs.Genbank.genbank import GenBank
+from Datasnakes.Orthologs.Align.alignment import Alignment
 
 
+class Blast2PAML(luigi.Task):
+    """Blasn to PAML."""
+    n = luigi.Parameter(default=10)
+
+    def requires(self):
+        return [ProjMana(),
+                BLASTn(),
+                GenBank(),
+                Alignment()]
+
+    def output(self):
+        return luigi.LocalTarget("numbers_up_to_{}.txt".format(self.n))
+
+    def run(self):
+        with self.output().open('w') as f:
+            for i in range(1, self.n+1):
+                f.write("{}\n".format(i))
 
 
-# Create directory structure
-
-# Get GI lists
-
-# Run blastn with given refseqrna accession #
-
-# Align sequences using guidance or clustal omega
-
-# Prepare sequences for
+if __name__ == '__main__':
+    # from luigi.mock import MockFile
+    luigi.run(["--local-scheduler"], main_task_cls='Blast2PAML')
