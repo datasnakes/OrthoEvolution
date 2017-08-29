14c82fe413575693c52fb23533717e37e4886fdb
rgilmore <rgilmore@umc.edu>
2017-08-29 14:18:01 -0500

    Fixed. Used tier_frame_dict.keys() instead of tier_frame_dict.items(). https://github.com/datasnakes/Datasnakes-Scripts/pull/44

--- Datasnakes/Orthologs/Genbank/utils.py
+++ Datasnakes/Orthologs/Genbank/utils.py
@@ -53,7 +53,7 @@ def dir_config(path, tier_frame_dict):
             --Gene_M+1
             --Gene_N
     """
-    for G_KEY, G_value in tier_frame_dict.items():
+    for G_KEY in tier_frame_dict.keys():
         tier = G_KEY
         tier_path = path / Path(tier)
         Path.mkdir(tier_path, parents=True, exist_ok=True)
