76bdf5a053836aedc6e3c8347eba227d80fd6fcc
rgilmore <rgilmore@umc.edu>
2017-08-29 13:01:00 -0500

    Brought Guidance2 and Pal2Nal wrappers to the front and added Alignment class.

--- Datasnakes/Orthologs/Align/QualityControl/__init__.py
+++
@@ -1,9 +0,0 @@
-"""Quality Control Alignment command line tool wrappers."""
-
-from .filter import FilteredAlignment
-from .guidance2 import Guidance2Commandline
-from .pal2nal import Pal2NalCommandline
-
-# Make this explicit, then they show up in the API docs
-__all__ = ("FilteredAlignment", "Guidance2Commandline", "Pal2NalCommandline"
-           )
