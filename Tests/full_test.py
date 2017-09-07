5d1b14efab68f1f8d9d0086370d77d63355376fe
rgilmore <rgilmore@umc.edu>
2017-09-07 16:17:03 -0500

    Class name changes.

--- Tests/full_test.py
+++ Tests/full_test.py
@@ -1,5 +1,5 @@
 from Datasnakes.Manager.utils.mana import ProjectManagement
-from Datasnakes.Orthologs.Blast.blastn import BLASTn
+from Datasnakes.Orthologs.Blast.blastn import CompGenBLASTn
 from Datasnakes.Orthologs.Genbank.genbank import GenBank
 from Datasnakes.Orthologs.Align.msa import MultipleSequenceAlignment
 
@@ -9,7 +9,7 @@ pm = ProjectManagement(repo='test-repo', user='test-user', project=project,
                        research='test-research', research_type='comparative_genetics',
                        new_repo=True, new_user=True, new_project=True, new_research=True)
 
-bn = BLASTn(project=project, copy_from_package=True, MAF='MAFV3.2.csv', proj_mana=pm)
+bn = CompGenBLASTn(project=project, copy_from_package=True, MAF='MAFV3.2.csv', proj_mana=pm)
 
 gb = GenBank(project=project, blast=bn)
 # al = MultipleSequenceAlignment(project=project, aln_program='GUIDANCE2', genbank=gb)
