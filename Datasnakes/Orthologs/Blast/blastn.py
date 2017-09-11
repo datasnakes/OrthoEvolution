f91f7efaa04c4020b878f446fd58b19917dad589
sdhutchins <sdhutchins@users.noreply.github.com>
2017-09-11 14:26:18 -0500

    Caught error in blastn.py for makedirectory

--- Datasnakes/Orthologs/Blast/blastn.py
+++ Datasnakes/Orthologs/Blast/blastn.py
@@ -31,7 +31,7 @@ class CompGenBLASTn(CompGenFiles):
         self.home = Path(os.getcwd())
         self.__gi_list_path = self.project_database / Path('gi_lists')
 
-        makedirectory(self.__gi_list_path, parents=True)
+        makedirectory(self.__gi_list_path)
 
         # # Initialize Logging
         # self.__blastn_log = LogIt.blastn()
