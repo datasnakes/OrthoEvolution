1859bc248c68f4f26bbb08820e4b4a6c1da08a2d
sdhutchins <sdhutchins@outlook.com>
2017-09-15 17:29:47 -0500

    Added parallel folder. Added Contributing.rst

---
+++ Datasnakes/Tools/parallel/multiprocess.py
@@ -0,0 +1,26 @@
+"""Use python's multiprocessing module to create multiple processes and speed
+up the completion of functions/classes."""
+from logzero import logger as log
+from multiprocessing import Pool, cpu_count
+from time import time
+import sys
+
+
+class Multiprocess(object):
+    def __init__(self, n, function, listinput):
+        self.n = n  # Number of processes to run
+
+        # Force user to use optimal number of processes
+        if int(self.n) > (int(cpu_count) - 1):
+            sys.exit(ValueError)
+
+        self.function = function
+        self.listinput = listinput
+
+    def main(self):
+        """Start a pool to run your function with a list."""
+        ts = time()
+        with Pool(processes=self.n) as p:
+            p.map(self.function, self.listinput)
+            minutes = (time() - ts) / 60
+            log.info("Took {} minutes to complete".format(minutes))
