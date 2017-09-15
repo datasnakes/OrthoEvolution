d3accd0d3d3236dcc4b4595832363cd06a7ac738
sdhutchins <sdhutchins@outlook.com>
2017-09-15 15:35:59 -0500

    Blank out manual imports in init

--- Datasnakes/Manager/__init__.py
+++ Datasnakes/Manager/__init__.py
@@ -1,13 +1,13 @@
-"""Utilities & Management classes"""
-
-from .management import Management, ProjectManagement, RepoManagement, UserManagement, WebsiteManagement
-from .database_management import DatabaseManagement
-
-
-__all__ = ("DatabaseManagement",
-           "WebsiteManagement",
-           "Management",
-           "ProjectManagement",
-           "RepoManagement",
-           "UserManagement",
-           )
+#"""Utilities & Management classes"""
+#
+#from .management import Management, ProjectManagement, RepoManagement, UserManagement, WebsiteManagement
+#from .database_management import DatabaseManagement
+#
+#
+#__all__ = ("DatabaseManagement",
+#           "WebsiteManagement",
+#           "Management",
+#           "ProjectManagement",
+#           "RepoManagement",
+#           "UserManagement",
+#           )
