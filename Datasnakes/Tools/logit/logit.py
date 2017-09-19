862b5a31f7058861198138e1e1f4b366bf85e2c8
sdhutchins <sdhutchins@outlook.com>
2017-09-19 03:01:57 -0500

    Revamped logit. Wrapped it around logzero.

--- Datasnakes/Tools/logit/logit.py
+++ Datasnakes/Tools/logit/logit.py
@@ -1,17 +1,12 @@
 """Main logging class to make logging easier."""
-import logging as log
+from logzero import setup_logger, LogFormatter, logging
 import os
-from datetime import datetime as d
 import sys
-# import configparser
-# from slacker import Slacker
-# import argparse
-# import textwrap
 
 
 class LogIt(object):
     """LogIt makes logging easier by creating easy loggers."""
-    def __init__(self, logfile=None, logname=None):
+    def __init__(self):
         """Initialize the logger format based on system platform."""
         # Set the different formats based on user's platform
         if sys.platform == 'win32':
@@ -19,49 +14,44 @@ class LogIt(object):
         elif sys.platform == 'linux':
             self.archive_format = '%m-%d-%Y@%I:%M:%S-%p'
 
-        self.date_format = '%a %b %d at %I:%M:%S %p %Y'  # Used to add date
-        self.log_format = '%(name)s - [%(levelname)-2s]: %(message)s'
-        # self.slack = self.slack_config()
-        self.basic = self.generic_logger(
-            logfile, logname, log.DEBUG, self.log_format)
+        self.date_format = '%b-%d-%Y at %I:%M:%S %p'  # Used to add date
+        self.log_format = '%(color)s[%(levelname)s | %(name)s] [%(asctime)s | %(module)s - line %(lineno)d]:%(end_color)s %(message)s'
+        self.formatter = LogFormatter(fmt=self.log_format,
+                                      datefmt=self.date_format)
 
-    def _get_file(self, filename):
-        """Create a log file."""
-        base, extension = filename.split('.')
-        file = base + str(d.now().strftime(self.archive_format)) + extension
-        path = os.getcwd() + file
-        return path
+        # TODO Allow customization
+#        if not default:
+#            self.custom_log = self.custom(logname, logfile, level, fmt)
 
-    def generic_logger(self, filename, logname, level, fmt, slack=False):
-        """Create a generic logger."""
-        file_path = self._get_file(filename)
-        log.basicConfig(level=level,
-                        format=fmt,
-                        filename=file_path)
-        generic_logger = log.getLogger(logname)
-        if slack is False:
-            return generic_logger
-#        else:
-#            slack_logger = log.getLogger('SLACK')
+    def default(self, logname, logfile):
+        """Create a log handler using default formatting."""
+        default_log = setup_logger(name=logname.upper(), logfile=logfile,
+                                   level=logging.DEBUG, formatter=self.formatter)
+        return default_log
 
+    def custom(self, logname, logfile, level, fmt='default'):
+        """Create a log handler or logger."""
+        if fmt is 'default':
+            fmt = '[%(levelname)-2s - %(name)s]: %(message)s'
+        elif fmt is 'custom':
+            fmt = self.formatcreator()
+        elif fmt is not 'default' or 'custom':
+            raise Exception('User did not provide a format for the logger.')
 
-    # @staticmethod
-    # def slack_config():
-    #     config = configparser.ConfigParser()
-    #     config.read('bin/orthologs.ini')
-    #     apikey = config['APIKEYS']['slack']
-    #     slack = Slacker(apikey)
-    #     return slack
-    #
-    # # Definition for uploading images
-    # def upload_img(self, channel, imgfile):
-    #     self.slack.files.upload(imgfile, channel=channel)
-    #
-    # # Definition for uploading files
-    # def upload_file(self, channel, file):
-    #     self.slack.files.upload(file, channel=channel)
-    #
-    # # Definition for posting messages
-    # def message_slack(self, channel, message, username):
-    #     self.slack.chat.post_message(channel, message, username, as_user=True)
+        custom_log = setup_logger(name=logname, logfile=logfile, level=level,
+                                  formatter=fmt)
+        return custom_log
 
+    def deletelog(self, logfile):
+        """Delete the log file."""
+        self.shutdown()
+        # TODO Use contextlib here
+        if os.path.exists(logfile):
+            os.remove(logfile)
+
+    def shutdown(self):
+        """Shutdown the log handlers."""
+        # HINT https://www.programcreek.com/python/example/3517/logging.shutdown
+        logging.shutdown()
+        # Windows won't just delete a log (linux distros will).
+        # It must be shutdown.
