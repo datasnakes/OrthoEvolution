21a11dfb884882802268c492fa876c48ec185040
sdhutchins <sdhutchins@outlook.com>
2017-09-21 09:18:01 -0500

    Made code style more pythonic. Added map2function classmethod

--- Datasnakes/Tools/parallel/multiprocess.py
+++ Datasnakes/Tools/parallel/multiprocess.py
@@ -1,26 +1,29 @@
 """Use python's multiprocessing module to create multiple processes and speed
 up the completion of functions/classes."""
-from logzero import logger as log
+
 from multiprocessing import Pool, cpu_count
 from time import time
 import sys
+from logzero import logger as log
 
 
 class Multiprocess(object):
-    def __init__(self, n, function, listinput):
-        self.n = n  # Number of processes to run
+    """Use multiple processes with functions."""
+    def __init__(self, num_procs, function, listinput):
+        self.num_procs = num_procs  # Number of processes to run
 
         # Force user to use optimal number of processes
-        if int(self.n) > (int(cpu_count) - 1):
+        if int(self.num_procs) > (int(cpu_count) - 1):
             sys.exit(ValueError)
 
         self.function = function
         self.listinput = listinput
 
-    def main(self):
+    @classmethod
+    def map2function(cls, num_procs, function, listinput):
         """Start a pool to run your function with a list."""
-        ts = time()
-        with Pool(processes=self.n) as p:
-            p.map(self.function, self.listinput)
-            minutes = (time() - ts) / 60
-            log.info("Took {} minutes to complete".format(minutes))
+        time_secs = time()
+        with Pool(processes=num_procs) as pool:
+            pool.map(function, listinput)
+            minutes = (time() - time_secs) / 60
+            log.info("Took %s minutes to complete.", minutes)
