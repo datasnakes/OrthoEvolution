334394c923d8d0ac603bc372671120f10c6e074e
sdhutchins <sdhutchins@users.noreply.github.com>
2017-10-03 17:05:07 -0500

    Made variables private.

--- Datasnakes/Tools/logit/logit.py
+++ Datasnakes/Tools/logit/logit.py
@@ -10,21 +10,21 @@ class LogIt(object):
         """Initialize the logger format based on system platform."""
         # Set the different formats based on user's platform
         if sys.platform == 'win32':
-            self.archive_format = '%m-%d-%Y_%I-%M-%p'
+            self._archive_format = '%m-%d-%Y_%I-%M-%p'
         elif sys.platform == 'linux':
-            self.archive_format = '%m-%d-%Y@%I:%M:%S-%p'
+            self._archive_format = '%m-%d-%Y@%I:%M:%S-%p'
 
-        self.date_format = '%b-%d-%Y at %I:%M:%S %p'  # Used to add date
-        self.log_format = ("%(color)s[%(levelname)s | %(name)s] [%(asctime)s | "
-                           "%(module)s - line %(lineno)d]:%(end_color)s %(message)s")
-        self.formatter = LogFormatter(fmt=self.log_format,
-                                      datefmt=self.date_format)
+        self._date_format = '%b-%d-%Y at %I:%M:%S %p'  # Used to add date
+        self._log_format = ("%(color)s[%(levelname)s | %(name)s] [%(asctime)s | "
+                            "%(module)s - line %(lineno)d]:%(end_color)s %(message)s")
+        self._formatter = LogFormatter(fmt=self._log_format,
+                                       datefmt=self._date_format)
         self.logging = logging
 
     def default(self, logname, logfile):
         """Create a log handler using default formatting."""
         default_log = setup_logger(name=logname.upper(), logfile=logfile,
-                                   level=logging.DEBUG, formatter=self.formatter)
+                                   level=logging.DEBUG, formatter=self._formatter)
         return default_log
 
     @classmethod
@@ -46,7 +46,7 @@ class LogIt(object):
         """Delete the log file."""
         self.shutdown()
         # TODO Use contextlib here; See makedirectory function
-        if os.path.exists(logfile) and os.path.isfile(logfile):
+        if os.path.isfile(logfile):
             os.remove(logfile)
 
     def shutdown(self):
