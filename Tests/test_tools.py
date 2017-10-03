1a40ba844f2110e10965a7baa1ca6d26438a9b90
sdhutchins <sdhutchins@outlook.com>
2017-10-03 10:08:49 -0500

    Wrote a shell test for tools

--- Tests/test_tools.py
+++ Tests/test_tools.py
@@ -1,1 +1,28 @@
 """This is the test suite for Tools."""
+
+import unittest
+from shutil import rmtree
+
+from Datasnakes.Tools import LogIt, Multiprocess
+
+
+
+class TestTools(unittest.TestCase):
+    """Test the Tools module."""
+
+    def setUp(self, project='test-project', repository='test-repo'):
+        self.project = project
+        self.repo = repository
+
+    def delete_repo(self):
+        rmtree(self.repo)
+
+    def test_logit(self):
+        print()
+
+    def test_multiprocess(self):
+        print()
+
+
+if __name__ == '__main__':
+    unittest.main()
