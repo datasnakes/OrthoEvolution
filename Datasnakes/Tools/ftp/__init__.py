f1a8f1ece67e00de9e69053197097632b2789969
sdhutchins <sdhutchins@users.noreply.github.com>
2017-10-03 16:17:19 -0500

    Created NCBIFTPClient class and inherited the BaseFTPClient

--- Datasnakes/Tools/ftp/__init__.py
+++ Datasnakes/Tools/ftp/__init__.py
@@ -1,2 +1,3 @@
 """Initialize FTP class"""
 from .baseftp import BaseFTPClient
+from .ncbiftp import NCBIFTPClient
--- Datasnakes/Tools/ftp/ncbiftp.py
+++ Datasnakes/Tools/ftp/ncbiftp.py
@@ -1,15 +1,15 @@
 # -*- coding: utf-8 -*-
-from baseftp import BaseFTP
+from baseftp import BaseFTPClient
 from ftplib import FTP
 from Datasnakes import DatasnakesDevelopmentWarning
 
 
-class NCBIFTP(BaseFTP):
+class NCBIFTPClient(BaseFTPClient):
     """Access NCBI's FTP servers with ease."""
     # raise DatasnakesDevelopmentWarning('NCBIFTP is not ready to be used.')
     def __init__(self, email, ftpsite='ftp.ncbi.nlm.nih.gov'):
-        BaseFTP.__init__(self, email, ftpsite)
+        super().__init__(ftpsite, email)
 
 
-# TODO Create function to download blast databases
+# TODO Create functions to download blast databases
 # TODO Create function to list databases
