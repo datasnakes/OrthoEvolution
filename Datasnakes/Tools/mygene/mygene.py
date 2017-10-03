5e103a1ac1e134c7cfd24808482ba0a186740e04
sdhutchins <sdhutchins@outlook.com>
2017-10-03 10:10:57 -0500

    Addded a mygene_temp file.

--- Datasnakes/Tools/mygene/mygene.py
+++ Datasnakes/Tools/mygene/mygene.py
@@ -18,8 +18,8 @@ class MyGene(object):
         Get the basic gene information. It's best to use a csv file and title
         the row of the accessions list `Accessions`.
         """
-        mygene_temp = pkg_resources.resource_filename(config.__name__,
-                                                      'mygenetemp.csv')
+#        mygene_temp = pkg_resources.resource_filename(config.__name__,
+#                                                      'mygenetemp.csv')
         self.infile = infile
         self.outfile = outfile
 
