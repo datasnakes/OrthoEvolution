b8b163de4866729a6350f5f14a77dd44622144ba
sdhutchins <sdhutchins@users.noreply.github.com>
2017-10-04 18:22:59 -0500

    Added template for remainder of class

--- Datasnakes/Tools/ftp/ncbiftp.py
+++ Datasnakes/Tools/ftp/ncbiftp.py
@@ -1,15 +1,23 @@
-# -*- coding: utf-8 -*-
+"""Class to access NCBI's ftp server and easily download databases."""
 from baseftp import BaseFTPClient
-from ftplib import FTP
-from Datasnakes import DatasnakesDevelopmentWarning
 
 
 class NcbiFTPClient(BaseFTPClient):
     """Access NCBI's FTP servers with ease."""
-    # raise DatasnakesDevelopmentWarning('NCBIFTP is not ready to be used.')
-    def __init__(self, email, ftpsite='ftp.ncbi.nlm.nih.gov'):
-        super().__init__(ftpsite, email)
+    def __init__(self, email):
+        _NCBI = 'ftp.ncbi.nlm.nih.gov'
+        super().__init__(ftpsite=_NCBI, email=email, keepalive=False, debug_lvl=0)
 
+    def list_blastdbs(self):
+        # TODO Create function to list databases
+        """List all blast databases."""
+        pass
 
-# TODO Create functions to download blast databases
-# TODO Create function to list databases
+    def getblastdb(self, database_name, unzip=True):
+        # TODO Create function to download blast databases
+        """Download the blast database."""
+        pass
+
+    def _unzip(self):
+        """Unzip blast databases."""
+        pass
