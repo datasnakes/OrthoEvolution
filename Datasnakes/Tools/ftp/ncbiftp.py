6efa3d233825bf29c778f7ee77fcc5bee4500c2f
sdhutchins <sdhutchins@users.noreply.github.com>
2017-10-05 17:44:15 -0500

    Added more shell functions to ncbiftp

--- Datasnakes/Tools/ftp/ncbiftp.py
+++ Datasnakes/Tools/ftp/ncbiftp.py
@@ -1,5 +1,8 @@
 """Class to access NCBI's ftp server and easily download databases."""
 from baseftp import BaseFTPClient
+import tarfile
+
+from Datasnakes.Tools.parallel import Multiprocess
 
 
 class NcbiFTPClient(BaseFTPClient):
@@ -7,17 +10,43 @@ class NcbiFTPClient(BaseFTPClient):
     def __init__(self, email):
         _NCBI = 'ftp.ncbi.nlm.nih.gov'
         super().__init__(ftpsite=_NCBI, email=email, keepalive=False, debug_lvl=0)
+        self.blastdb_path = '/blast/db/'
+        self.blastfasta_path = '/blast/db/FASTA'
+        blastdbs = []
+        blastfastadbs = []
 
-    def list_blastdbs(self):
-        # TODO Create function to list databases
-        """List all blast databases."""
-        pass
+    class get:
+        def blastdb(self, database_name, extract=True):
+            """Download the preformatted blast database."""
+            pass
 
-    def getblastdb(self, database_name, unzip=True):
-        # TODO Create function to download blast databases
-        """Download the blast database."""
-        pass
+        def blastfasta(self, database_name, extract=True):
+            """Download the fasta sequence database (not formatted)."""
 
-    def _unzip(self):
+    def _extract(self):
         """Unzip blast databases."""
+        pass
+
+    def _download_file(self, filename):
+        """Download the files one by one."""
+        self.ftp.retrbinary("RETR " + filename, open(filename, 'wb').write)
+
+    def _multi_download(self):
+        """Download the files using multiprocessing."""
+        Multiprocess(num_procs=4, function=self._download_file,
+                     listinput='')
+
+    def _listall(self, path):
+        if path == 'blastdb':
+            self.ftp.cwd(self.blastdb_path)
+            blastdbs
+            return
+        elif path == 'blastfasta':
+            self.ftp.cwd(self.blastdb_path)
+        else:
+            raise Exception
+
+        filenames = self.ftp.nlst()
+
+    def _parseMLSD(self):
         pass
