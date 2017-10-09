eae953b4f33c5f56ee037e347ac3d4deb9ab8cc5
sdhutchins <sdhutchins@users.noreply.github.com>
2017-10-09 16:42:16 -0500

    Added back Multiprocess class.

--- Datasnakes/Tools/parallel/multiprocess.py
+++ Datasnakes/Tools/parallel/multiprocess.py
@@ -8,29 +8,32 @@ import logging
 import logzero
 
 
-cpus = cpu_count()
-num_procs = cpus - 1
+class Multiprocess(object):
+    """Use multiple processes with a function."""
+    cpus = cpu_count()
+    num_procs = cpus - 1
 
+    def __init__(self):
+        pass
 
-def _logger():
-    """Add the multiprocessing module's logger."""
-    multiprocess_handler = get_logger()
-    multiprocess_handler = logging.StreamHandler()
-    multiprocess_handler.setLevel(logging.ERROR)
-    multiprocess_handler.setFormatter(logzero.LogFormatter(color=True))
+    def _logger(self):
+        """Add the multiprocessing module's logger."""
+        multiprocess_handler = get_logger()
+        multiprocess_handler = logging.StreamHandler()
+        multiprocess_handler.setLevel(logging.ERROR)
+        multiprocess_handler.setFormatter(logzero.LogFormatter(color=True))
 
-    # Attach it to the logzero default logger
-    logzero.logger.addHandler(multiprocess_handler)
-    logger = logzero.logger
-    return logger
+        # Attach it to the logzero default logger
+        logzero.logger.addHandler(multiprocess_handler)
+        logger = logzero.logger
+        return logger
 
-
-def map2function(function, listinput):
-    """Start a pool to run your function with a list."""
-    log = _logger()  # Start the logger
-    time_secs = time()
-    with Pool(processes=num_procs) as pool:
-        pool.map(function, listinput)
-        minutes = (time() - time_secs) / 60
-    log.info("Took %s minutes to complete.", minutes)
-    logging.shutdown()  # Shutdown the logger
+    def map2function(self, function, iterable):
+        """Start a pool to run your function with a list."""
+        log = self._logger()  # Start the logger
+        time_secs = time()
+        with Pool(processes=self.num_procs) as pool:
+            pool.map(function, iterable)
+            minutes = (time() - time_secs) / 60
+        log.info("Took %s minutes to complete.", minutes)
+        logging.shutdown()  # Shutdown the logger
