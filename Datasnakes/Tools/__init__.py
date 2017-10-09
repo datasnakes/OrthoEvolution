dc917b2165b2b8b7f60879efc180a9fe30800903
sdhutchins <sdhutchins@users.noreply.github.com>
2017-10-09 13:09:56 -0500

    Dissolved Multiprocess class

--- Datasnakes/Tools/__init__.py
+++ Datasnakes/Tools/__init__.py
@@ -1,6 +1,6 @@
 # TODO-SDH Consider dissolving the folders.
 from Datasnakes.Tools.logit import LogIt
-from Datasnakes.Tools.parallel import Multiprocess
+from Datasnakes.Tools.parallel import map2function, num_procs
 from Datasnakes.Tools.slackify import Slackify
 from Datasnakes.Tools.utils import (formatlist, splitlist, makedirectory,
                                     PackageVersion)
