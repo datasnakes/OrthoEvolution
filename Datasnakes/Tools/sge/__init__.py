575a785d3346f8bb53e3da501088e8b6c383b746
sdhutchins <sdhutchins@outlook.com>
2017-10-19 17:50:52 -0500

    Removed sgeconfig form init

--- Datasnakes/Tools/sge/__init__.py
+++ Datasnakes/Tools/sge/__init__.py
@@ -2,7 +2,6 @@ import platform
 from .sgeutils import randomid, basejobids, import_temp, writecodefile, file2str
 from .qstat import Qstat
 from .sgejob import SGEJob
-from .sgeconfig import __DEFAULT__, __CUSTOM__
 from .sgepipelinetask import SGEPipelineTask
 
 
