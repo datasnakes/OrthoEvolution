c686067e433728396eb70f1201f504e78dbe553a
sdhutchins <sdhutchins@outlook.com>
2017-10-19 16:45:59 -0500

    Updated init for sge submodule

--- Datasnakes/Tools/sge/__init__.py
+++ Datasnakes/Tools/sge/__init__.py
@@ -1,2 +1,10 @@
-from .qsub_utils import Qsubutils
+import platform
+from .sgeutils import randomid, basejobids, import_temp, writecodefile, file2str
 from .qstat import Qstat
+from .sgejob import SGEJob
+from .sgeconfig import __DEFAULT__, __CUSTOM__
+from .sgepipelinetask import SGEPipelineTask
+
+
+if "windows" in platform.system().lower():
+    raise ImportError("This module is only supported on linux/osx.")
