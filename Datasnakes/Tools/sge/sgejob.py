eaa4cc64e8bf725d37aeee69a56b85aa9798f5cf
sdhutchins <sdhutchins@outlook.com>
2017-10-19 17:50:00 -0500

    Adding configuration. Simplified submit

--- Datasnakes/Tools/sge/sgejob.py
+++ Datasnakes/Tools/sge/sgejob.py
@@ -4,6 +4,7 @@ import os
 
 from Datasnakes.Tools.sge import basejobids, writecodefile, import_temp
 
+
 class BaseJob(object):
     """Create a class for simple jobs."""
     def __init__(self):
@@ -18,32 +19,25 @@ class SGEJob(BaseJob):
     """Create multiple jobs & scripts for each job to run based on
     splitting a list into chunks.
     """
-    def __init__(self):
-        pass
+    def __init__(self, jobname):
+        self.baseid, self.base = self.basejobids(jobname)
     
-    def configure(self):
-        pass
+    def configure(self, jobname):
+        """Configure job attributes or set it up."""
+        baseid, base = basejobids(jobname)
     
-    def submit(self, code, default=True, prefix=None):
+    def submit(self, code, default=True):
         """Creates and submit a qsub job. Also uses python code."""
         # TIP If python is in your environment as only 'python' update that.
         # TODO-SDH add a default parameter or custom parameter
         # If default, a python file will be created from code that is used.
         if self.default == default:
-            baseid, base = basejobids()
-            if prefix is not None:
-                base = prefix + '_' + base
-            writecodefile(filename=base, code=code, language='python')
-            outfile = 'orthoevol_{}.out'.format(baseid)
-            errfile = 'orthoevol_{}.err'.format(baseid)
+            writecodefile(filename=self.base, code=code, language='python')
+
             # Create the pbs script from the template or dict
             pbstemp = import_temp('temp.pbs')
-        
-            script_name = base.format(baseid)
-        
-            attributes = self.pbs_dict(outfile=outfile,
-                                       errfile=errfile,
-                                       script_name=script_name)
+                
+            attributes = __DEFAULT__
         
             with open(base + '.pbs', 'w') as pbsfile:
                 pbsfile.write(pbstemp.substitute(attributes))
@@ -57,7 +51,7 @@ class SGEJob(BaseJob):
         #                pbsfile.close()
         
         with contextlib.suppress(CalledProcessError):
-            cmd = ['qsub ' + base + '.pbs']  # this is the command
+            cmd = ['qsub ' + self.base + '.pbs']  # this is the command
             # Shell MUST be True
             cmd_status = run(cmd, stdout=PIPE, stderr=PIPE, shell=True)
         
