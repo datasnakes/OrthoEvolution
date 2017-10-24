351177badb9838bb4fa80bad1251c2efa63d5a50
robear22890 <robgilmore127@gmail.com>
2017-10-24 17:53:54 -0500

    Added methods for loading ncbi taxonomy to BioSQL database.

--- Datasnakes/Manager/BioSQL/biosql.py
+++ Datasnakes/Manager/BioSQL/biosql.py
@@ -8,33 +8,56 @@ from Datasnakes.Manager.BioSQL.biosql import scripts as sql_scripts
 
 
 class BaseBioSQL(object):
+    # TODO-ROB:  Organize the BioSQL files by driver/RDBMS
  # driver "mysql", "Pg", "Oracle", "SQLite"
     def __init__(self, database_name, database_type, driver):
         self.database_name = database_name
         self.database_type = database_type
         self.driver = driver
         self.biosqllog = LogIt().default(logname="BioSQL", logfile=None)
+
+        self.ncbi_taxon_script = pkg_resources.resource_filename(sql_scripts.__name__, "load_ncbi_taxonomy.pl")
         pass
 
     def load_biosql_schema(self, cmd, schema_file):
-        biosql_cmd = "%s < %s" % (cmd, schema_file)
-        biosql_load = subprocess.Popen([biosql_cmd], stderr=subprocess.PIPE, stdout=subprocess.PIPE, shell=True,
+        schema_cmd = "%s < %s" % (cmd, schema_file)
+        schema_load = subprocess.Popen([schema_cmd], stderr=subprocess.PIPE, stdout=subprocess.PIPE, shell=True,
                                        encoding='utf-8')
-        error = biosql_load.stderr.readlines()
-        out = biosql_load.stdout.readlines()
+        error = schema_load.stderr.readlines()
+        out = schema_load.stdout.readlines()
 
-        self.biosqllog.info("Error: " + str(error))
-        self.biosqllog.info("Out: " + str(out))
+        self.biosqllog.info("Schema-Error: " + str(error))
+        self.biosqllog.info("Schema-Out: " + str(out))
+        return error, out
 
-    def load_ncbi_taxonomy(self):
+    def load_ncbi_taxonomy(self, cmd):
+        "./load_ncbi_taxonomy.pl --dbname bioseqdb --driver mysql --dbuser root --download true"
+        taxon_cmd = cmd
+        taxon_load = subprocess.Popen([taxon_cmd], stderr=subprocess.PIPE, stdout=subprocess.PIPE, shell=True,
+                                      encoding='utf-8')
+
+        error = taxon_load.stderr.readlines()
+        out = taxon_load.stdout.readlines()
+
+        self.biosqllog.info("Taxon-Error: " + str(error))
+        self.biosqllog.info("Taxon-Out: " + str(out))
+        return error, out
         pass
 
 
 class SQLiteBioSQL(BaseBioSQL):
     def __init__(self, database_name):
         super().__init__(database_name=database_name, driver="SQLite")
-        self.cmd = "sqlite3 %s -echo" % database_name
+        self.schema_cmd = "sqlite3 %s -echo" % database_name
         self.schema_file = "biosqldb-sqlite.sql"
 
+        self.taxon_cmd = "%s --dbname %s --driver %s --download true" % \
+                         (self.ncbi_taxon_script, database_name, self.driver)
+
     def sqlite_schema(self):
-        self.load_biosql_schema(self.cmd, self.schema_file)
+        error, out = self.load_biosql_schema(self.schema_cmd, self.schema_file)
+        # TODO-ROB: Parse output and error
+
+    def sqlite_taxonomy(self):
+        error, out = self.load_ncbi_taxonomy(self.taxon_cmd)
+        # TODO-ROB: Parse output and error
