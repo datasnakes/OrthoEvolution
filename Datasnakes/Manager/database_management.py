00a88d87a01d1dbecc9257db0cd859d59b451bdd
robear22890 <robgilmore127@gmail.com>
2017-10-24 23:07:18 -0500

    Updated the get_taxonomy_database method for BioSQL databases.

--- Datasnakes/Manager/database_management.py
+++ Datasnakes/Manager/database_management.py
@@ -53,7 +53,11 @@ class DatabaseManagement(object):
             ete3 = import_module("ete3")
             ete3.NCBITaxa.update_taxonomy_database()
         elif db_type == 'ncbi':
-            print('ncbi')
+            # ftp://ftp.ncbi.nlm.nih.gov/pub/taxonomy
+            db_path = self.ncbi_db_repo / Path('pub') / Path('taxonomy')
+            biosql = import_module("Datasnakes.Manager.BioSQL.biosql")
+            ncbi_db = biosql.SQLiteBioSQL(database_name=db_path)
+            ncbi_db.create_taxonomy_database(db_path)
         elif db_type == 'biosql_repo':
             print('biosql_repo')
 
