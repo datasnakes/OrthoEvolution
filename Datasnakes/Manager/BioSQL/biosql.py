d265a83c8157e18c0c77b2ecd30b1fc669c62f62
rgilmore <rgilmore@umc.edu>
2017-10-26 15:31:37 -0500

    Finished docstrings and comments.

--- Datasnakes/Manager/BioSQL/biosql.py
+++ Datasnakes/Manager/BioSQL/biosql.py
@@ -35,10 +35,11 @@ class BaseBioSQL(object):
         """
         This script is a framework for loading the various schemas, the NCBI taxonomy (biosql-db), and the ITIS
         taxonomy (phylo-db) into a database.
-        :param cmd:
-        :param schema_file:
-        :return:
+        :param cmd:  The bash command to use.
+        :param schema_file:  The schema file for creating a BioSQL or PhyloDB
+        :return:  Returns the Output and the Error messages.
         """
+        # Build the command
         # To upload a schema, a schema file will be necessary.
         if schema_file:
             _ = "Schema"
@@ -47,16 +48,20 @@ class BaseBioSQL(object):
         else:
             _ = "Taxonomy"
             cmd = cmd
+
         # Run the perl scripts or sql scripts
         _loader = subprocess.Popen([cmd], stderr=subprocess.PIPE, stdout=subprocess.PIPE, shell=True, encoding='utf-8')
-        error = _loader.stderr.readlines()
         out = _loader.stdout.readlines()
+        error = _loader.stderr.readlines()
 
         self.biosqllog.info(_ + "-Error: " + str(error))
         self.biosqllog.info(_ + "-Out: " + str(out))
-        return error, out
+        return out, error
 
     def create_executable_scripts(self):
+        """
+        Changes the permissions of the BioSQL perl scripts, so that they are executable from the command line.
+        """
         # Set up the permissions for the BioSQL Perl scripts
         biosql_scripts = self.scripts
         for file in os.listdir(biosql_scripts):
@@ -68,44 +73,71 @@ class BaseBioSQL(object):
 
 
 class SQLiteBioSQL(BaseBioSQL):
-    def __init__(self, database_name, template="Template-BioSQL-SQLite.db"):
+    def __init__(self, database_name="Template-BioSQL-SQLite.db"):
+        """
+        This class inherits the BaseBioSQL class.  It uses the base methods to load schema, load taxonomy (NCBI, ITIS),
+        and create/copy template sqlite databases loaded with biosql schema and/or taxonomy data.
+        :param database_name:  The name of the database.
+        :param template:  The standard template name.
+        """
         super().__init__(database_name=database_name, driver="SQLite")
         self.schema_cmd = "sqlite3 %s -echo"
         self.schema_file = "biosqldb-sqlite.sql"
         self.taxon_cmd = "%s --dbname %s --driver %s --download true"
 
-        self.template = template
-
-    def load_sqlite_schema(self, database_name=None):
+    def load_sqlite_schema(self):
+        """
+        Loads an SQLite biosql schema into a database file.
+        """
+        # Build the command
         schema_file = pkg_resources.resource_filename(sql.__name__, self.schema_file)
-        if database_name:
-            self.database_name = database_name
         schema_cmd = self.schema_cmd % self.database_name
-        error, out = self.configure_new_database(schema_cmd, schema_file)
-        return error, out
+        # Run the bash command
+        out, error = self.configure_new_database(schema_cmd, schema_file)
+        return out, error
         # TODO-ROB: Parse output and error
 
-    def load_sqlite_taxonomy(self, database_name=None):
-        if database_name:
-            self.database_name = database_name
+    def load_sqlite_taxonomy(self):
+        """
+        Load an SQLite biosql database with taxonomy information.  This will only work for NCBI.  There is not any
+        support for the SQLite PhyloDB.
+        """
+        # Build the command
         taxon_cmd = self.taxon_cmd % (self.ncbi_taxon_script, self.database_name, self.driver)
-        error, out = self.configure_new_database(taxon_cmd)
-        return error, out
+        # Run the bash command
+        out, error = self.configure_new_database(taxon_cmd)
+        return out, error
         # TODO-ROB: Parse output and error
 
     def create_template_database(self, db_path):
-        db_path = Path(db_path) / Path(self.template)
+        """
+        Creates a template database by uploading SQLite schema and NCBI taxonomy.
+        :param db_path:  The relative path of the database.
+        :return:
+        """
+        db_path = Path(db_path) / Path(self.database_name)
+        # Create a template if it doesn't exits.
         if not db_path.is_file():
-            self.load_sqlite_schema(database_name=db_path)
+            self.load_sqlite_schema()
             self.create_executable_scripts()
-            self.load_sqlite_taxonomy(database_name=db_path)
+            # TODO-ROB:  Download the file manually and then use qsub job
+            self.load_sqlite_taxonomy()
         else:
-            self.biosqllog.warning("The template, %s, already exists." % self.template)
+            self.biosqllog.warning("The template, %s, already exists." % self.database_name)
 
     def copy_template_database(self, db_path, dest_path, dest_name=""):
-        db_path = Path(db_path) / Path(self.template)
+        """
+        This method copies a template sqlite biosql database.
+        :param db_path:  The relative path of the template database.
+        :param dest_path:  The path to copy the template into
+        :param dest_name:  The name of the new database file.
+        :return:  A new copy of the biosql database.
+        """
+        # If the template doesn't exists, then create it.
+        db_path = Path(db_path) / Path(self.database_name)
         if not db_path.is_file():
             self.create_template_database(db_path=db_path.parent)
+        # Copy the template into a new folder.
         dest_path = Path(dest_path) / Path(dest_name)
         self.biosqllog.warn('Copying Template BioSQL Database...  This may take a few minutes...')
         shutil.copy2(str(db_path), str(dest_path))
