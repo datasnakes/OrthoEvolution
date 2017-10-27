0348fc5cf8d7814562c515e9fe3bff54e9d8890a
rgilmore <rgilmore@umc.edu>
2017-10-27 17:50:59 -0500

    Added an uploading method framework to db_management.

--- Datasnakes/Manager/database_management.py
+++ Datasnakes/Manager/database_management.py
@@ -5,22 +5,26 @@ from Datasnakes.Manager import ProjectManagement
 from Datasnakes.Orthologs.utils import attribute_config
 from Datasnakes.Tools.ftp import NcbiFTPClient
 from Datasnakes.Tools.logit import LogIt
+from Datasnakes.Manager.BioSQL import biosql
+
 
 
 class DatabaseManagement(object):
 
-    def __init__(self, project, email, project_path=None, proj_mana=ProjectManagement, **kwargs):
+    def __init__(self, project, email, project_path=None, proj_mana=ProjectManagement, biosql_mana=biosql, **kwargs):
         self.dbmanalog = LogIt().default(logname="DatabaseManagement", logfile=None)
         self.config_options = {
             "GI_config": self.get_gi_lists,
             "Blast_config": self.download_blast_database,
             "Taxonomy_config": self.download_taxonomy_database,
-            "GenBank_config": self.download_refseq_release_files
+            "GenBank_config": [self.download_refseq_release_files, self.upload_genbank_flatfiles]
                                }
         self.project = project
         self.email = email
         self.database_dict = {}
         self.ncbiftp = NcbiFTPClient(email=self.email)
+        # TODO-ROB:  Configure this differently somehow
+        self.biosql = biosql_mana
 
         # Configuration of class attributes.
         add_self = attribute_config(self, composer=proj_mana, checker=ProjectManagement, project=project, project_path=project_path)
@@ -72,15 +76,14 @@ class DatabaseManagement(object):
             ete3.NCBITaxa.update_taxonomy_database()
         elif db_type == 'biosql':
             # Loads data from NCBI via ftp://ftp.ncbi.nlm.nih.gov/pub/taxonomy
-            biosql = import_module("Datasnakes.Manager.BioSQL.biosql")
             if driver.lower() == "sqlite3":
                 db_path = self.ncbi_db_repo / Path('pub') / Path('taxonomy')
-                ncbi_db = biosql.SQLiteBioSQL(database_path=db_path)
+                ncbi_db = self.biosql.SQLiteBioSQL(database_path=db_path)
                 ncbi_db.copy_template_database(dest_name=dest_name, dest_path=dest_path)
 
             elif driver.lower() == "mysql":
                 db_path = self.ncbi_db_repo / Path('pub') / Path('taxonomy')
-                ncbi_db = biosql.MySQLBioSQL()
+                ncbi_db = self.biosql.MySQLBioSQL()
 
         elif db_type == 'phylodb':
             # Loads data from ITIS via http://www.itis.gov/downloads/
@@ -88,15 +91,18 @@ class DatabaseManagement(object):
 
     # TODO-ROB:  Update ncbiftp class syntax to reflect NCBI's ftp site
     def download_refseq_release_files(self, database_name, database_path, collection_subset, seqtype, format, driver="sqlite3", extension=".gbk.db"):
-        db_name = str(database_name) + str(extension)
         db_path = self.ncbi_db_repo / Path('refseq') / Path('release') / Path(collection_subset)
         self.ncbiftp.getrefseqrelease(database_name=collection_subset, seqtype=seqtype, filetype=format, download_path=db_path)
         if database_path:
             db_path = Path(database_path)
-        self.download_taxonomy_database(db_type="biosql", dest_name=db_name, dest_path=db_path, driver=driver)
 
-    def upload_genbank_flatfiles(self, ):
+    def upload_refseq_release_files(self, database_name, database_path, collection_subset, seqtype, format,
+                                    driver="sqlite3", extension=".gbk.db"):
+        db_name = str(database_name) + str(extension)
+        db_path = self.ncbi_db_repo / Path('refseq') / Path('release') / Path(collection_subset)
+        self.download_taxonomy_database(db_type="biosql", dest_name=db_name, dest_path=db_path, driver=driver)
         pass
+
     def get_project_genbank_database(self):
         print()
 
