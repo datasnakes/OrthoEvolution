8ed6f64d3f878b2ea7c82ae1b73b78582a4d187a
sdhutchins <sdhutchins@outlook.com>
2017-10-29 22:44:11 -0500

    Added a function that will parse output of qstat.

--- Datasnakes/Tools/sge/qstat.py
+++ Datasnakes/Tools/sge/qstat.py
@@ -5,45 +5,54 @@ import getpass
 
 class Qstat(object):
     def __init__(self):
-        """
-        Parameters
-        ----------
-        qstat_path : string
-            The path to the qstat executable.
-        Returns
-        -------
-        queue_info : list
-            A list of jobs in 'queue_info'. Jobs are dictionaries with both
-            string keys and string names.
-        job_info : list
-            A list of jobs in 'job_info'.
-        """
-        _username = getpass. getuser()
+        """Initialize class."""
+        _username = getpass.getuser()
         self.username = _username
         self.qstatinfo()
-        
 
-    @classmethod
-    def qstatinfo(cls, qstat_path='qstat', option=''):
-        """
-        Parameters
-        ----------
-        qstat_path : string
-            The path to the qstat executable.
-        Returns
-        -------
-        qstatxml : string
-            The xml stdout string of the 'qstat -xml' call.
-        """
+
+    def qstatinfo(self, qstat_path='qstat', option='-u'):
+        """Retrieve qstat output."""
         try:
-            qstatinfo = run([qstat_path, option], stdout=PIPE, stderr=PIPE, shell=True)
+            qstatinfo = run([qstat_path, option, self.username],
+                            stdout=PIPE, stderr=PIPE, shell=True)
         except CalledProcessError as cpe:
             return_code = 'qstat returncode: %s' % cpe.returncode
             std_error = 'qstat standard output: %s' % cpe.stderr
             print(return_code + '\n' + std_error)
         except FileNotFoundError:
             raise FileNotFoundError('qstat is not on your machine.')
-        
-        return qstatinfo.stdout
+
+        jobs = self._output_parser(qstatinfo.stdout)
+
+        self.running_jobs = sum(j['status'] in ['R', 'Q'] for j in jobs)
+
+        return self.running_jobs
 
 # TODO Add function that will parse output of qstat
+    def _output_parser(self, output):
+        """
+        Parse output from qstat pbs commandline program
+
+        Arguments:
+        ----------
+        output: str, output obtained from qstat command
+        Returns:
+        ---------
+        list, of all jobs. Each job is represented as a dictionary conainting
+            all relevant information.
+        """
+        lines = output.split('\n')
+        del lines[:5]
+        jobs = []
+        for line in lines:
+            els = self.split_regex.split(line)
+            try:
+            	j = {"id_": els[0], "user": els[1], "queue": els[2], "name": els[3],
+                 	"status": els[9], "elapsed_time": els[10]}
+            	jobs.append(j)
+
+            except IndexError:
+                pass
+
+        return jobs
