2235b3b108808919cb2227091e4e8170ca5bef6f
sdhutchins <sdhutchins@outlook.com>
2017-11-01 17:15:57 -0500

    Updated init of Blast

--- Datasnakes/Orthologs/Blast/__init__.py
+++ Datasnakes/Orthologs/Blast/__init__.py
@@ -2,6 +2,8 @@ from .blastn_comparative_genetics import CompGenBLASTn
 from .comparative_genetics_files import CompGenFiles
 from .comparative_genetics_objects import CompGenObjects
 
+
+    
 # Make this explicit, then they show up in the API docs
 __all__ = ("CompGenObjects",
            "CompGenFiles",
