81867ef2048b07556f819d48eeda59d21be63fb8
rgilmore <rgilmore@umc.edu>
2017-11-02 10:49:13 -0500

    Created the StreamIEO class.

--- Datasnakes/Tools/streamieo/streamieo.py
+++ Datasnakes/Tools/streamieo/streamieo.py
@@ -1,41 +1,55 @@
 from subprocess import Popen, PIPE
 from threading import Thread
 from queue import Queue, Empty
-io_q = Queue()
-
+from Datasnakes.Tools.logit import LogIt
 # Implemented from http://www.sharats.me/posts/the-ever-useful-and-neat-subprocess-module/
 # In the "Watching both stdout and stderr" section
 
 
-def stream_watcher(identifier, stream):
+class StreamIEO(object):
 
-    for line in stream:
-        io_q.put((identifier, line))
+    def __init__(self):
+        """
+        A class that individually threads the capture of the stdout stream and the stderr stream of a system command.
+        The stdout/stderr are queued in the order they occur.  As the que populates another thread, parse the que and
+        prints to the screen using the LogIT class.
+        """
+        self.io_q = Queue()
+        self.streamieolog = LogIt().default(logname="streamieo", logfile=None)
 
-    if not stream.closed:
-        stream.close()
+    def streamer(self, cmd):
+        process = Popen([cmd], stdout=PIPE, stderr=PIPE)
+        # Watch the standard output and add it to the que
+        Thread(target=self._stream_watcher, name='stdout-watcher', args=('STDOUT', process.stdout)).start()
+        # Watch the standard input and add it to the que
+        Thread(target=self._stream_watcher, name='stderr-watcher', args=('STDERR', process.stderr)).start()
+        # As items are added, print the stream.
+        Thread(target=self._printer, name='_printer', args=process).start()
 
+    def _stream_watcher(self, identifier, stream):
+        # Watch the stream and add to the que dynamically
+        # This runs in tandem with the printer.  So as the stdout/stderr streams are queued here,
+        # the que is parsed and printed in the printer function.
+        for line in stream:
+            self.io_q.put((identifier, line))
+        if not stream.closed:
+            stream.close()
 
-proc = Popen('svn co svn+ssh://myrepo', stdout=PIPE, stderr=PIPE)
-
-Thread(target=stream_watcher, name='stdout-watcher',
-       args=('STDOUT', proc.stdout)).start()
-Thread(target=stream_watcher, name='stderr-watcher',
-       args=('STDERR', proc.stderr)).start()
-
-
-def printer():
-    while True:
-        try:
-            # Block for 1 second.
-            item = io_q.get(True, 1)
-        except Empty:
-            # No output in either streams for a second. Are we done?
-            if proc.poll() is not None:
-                break
-        else:
-            identifier, line = item
-            print(identifier + ':', line)
-
-
-Thread(target=printer, name='printer').start()
+    def _printer(self, process):
+        # Prints the que as it is populated with stdout/stderr dynamically.
+        while True:
+            try:
+                # Block for 1 second.
+                item = self.io_q.get(True, 1)
+            except Empty:
+                # No output in either streams for a second. Are we done?
+                if process.poll() is not None:
+                    break
+            else:
+                identifier, line = item
+                if identifier == "STDERR":
+                    self.streamieolog.error(line)
+                elif identifier == "STDOUT":
+                    self.streamieolog.info(line)
+                else:
+                    self.streamieolog.critical(identifier + ':' + line)
