de90d4ad01eec588e35fbd348d0a32a22f1651e8
rgilmore <rgilmore@umc.edu>
2017-11-02 09:00:08 -0500

    Updated development print.

--- Datasnakes/Manager/BioSQL/biosql.py
+++ Datasnakes/Manager/BioSQL/biosql.py
@@ -72,9 +72,10 @@ class BaseBioSQL(object):
             cmd = cmd
 
         # Run the perl scripts or sql scripts
+        print("cmd: " + str(cmd))
         _loader = subprocess.Popen([cmd], stderr=subprocess.PIPE, stdout=subprocess.PIPE, shell=True, encoding='utf-8')
-        out = _loader.stdout.readlines()
-        error = _loader.stderr.readlines()
+        out = _loader.stdout.read()
+        error = _loader.stderr.read()
 
         self.biosqllog.info(_ + "-Error: " + str(error))
         self.biosqllog.info(_ + "-Out: " + str(out))
