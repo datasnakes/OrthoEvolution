124765863ba4109b87912115b80a38cb588083f6
sdhutchins <sdhutchins@users.noreply.github.com>
2017-11-02 20:43:00 -0500

    Removed whitespace in blast init

--- Datasnakes/Orthologs/Blast/__init__.py
+++ Datasnakes/Orthologs/Blast/__init__.py
@@ -3,7 +3,6 @@ from .comparative_genetics_files import CompGenFiles
 from .comparative_genetics_objects import CompGenObjects
 
 
-    
 # Make this explicit, then they show up in the API docs
 __all__ = ("CompGenObjects",
            "CompGenFiles",
