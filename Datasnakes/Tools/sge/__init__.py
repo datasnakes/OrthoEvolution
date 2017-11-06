a5b7d760280e117793648225f6911b1689695d7f
sdhutchins <sdhutchins@outlook.com>
2017-11-06 10:19:04 -0600

    Added Warning to sge init

--- Datasnakes/Tools/sge/__init__.py
+++ Datasnakes/Tools/sge/__init__.py
@@ -3,7 +3,10 @@ from .sgeutils import randomid, basejobids, import_temp, writecodefile, file2str
 from .qstat import Qstat
 from .sgejob import SGEJob
 from .sgepipelinetask import SGEPipelineTask
+from Datasnakes import DatasnakesWarning
 
 
 if "windows" in platform.system().lower():
     raise ImportError("This module is only supported on linux/osx.")
+else:
+    DatasnakesWarning('PBS professional must be installed on your system.')
