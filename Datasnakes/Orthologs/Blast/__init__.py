3efc537b4d30e19f0d634b963324cc641a038722
sdhutchins <sdhutchins@outlook.com>
2017-11-07 11:40:48 -0600

    Changed name of organisms and taxonomy ids file. Updated blast init

--- Datasnakes/Orthologs/Blast/__init__.py
+++ Datasnakes/Orthologs/Blast/__init__.py
@@ -1,10 +1,13 @@
 from .orthologs_blastn import OrthoBlastN
 from .comparative_genetics_files import CompGenFiles
 from .comparative_genetics_objects import CompGenObjects
-
+from Datasnakes.Orthologs import OrthologsWarning
+import warnings
 
 # Make this explicit, then they show up in the API docs
 __all__ = ("CompGenObjects",
            "CompGenFiles",
            "OrthoBlastN"
            )
+
+warnings.warn('Ensure that `BLASTDB` and `WINDOW_MASKER_PATH` are set as environment variables.',OrthologsWarning)
---
+++ Datasnakes/Tools/ftp/test.py
@@ -0,0 +1,10 @@
+import os
+from Datasnakes.Tools.ftp import NcbiFTPClient
+from Datasnakes.Tools.utils import csvtolist
+
+file = '/ddn/home5/r2295/src/datasnakes/Datasnakes/Manager/config/organisms_and_taxonomy_ids.csv'
+ids = csvtolist(file, 'Taxonomy ID')
+
+ncbiftp = NcbiFTPClient(email='shutchins2@umc.edu')
+path = '/ddn/home3/vallender/databases/window-masker-files/'
+ncbiftp.getwindowmaskerfiles(taxonomy_ids=ids, download_path=path)
