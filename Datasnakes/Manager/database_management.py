f0e48f0799462344f1a619f86058faf78c64ba64
robear22890 <robgilmore127@gmail.com>
2017-11-14 23:41:01 -0600

    Updated the database path and broke dl_taxon_db into individual functions.

--- Datasnakes/Manager/database_management.py
+++ Datasnakes/Manager/database_management.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 import yaml
 import requests
+import tarfile
 from importlib import import_module
 from Datasnakes.Manager.management import ProjectManagement
 from Datasnakes.Orthologs.utils import attribute_config
@@ -69,45 +70,36 @@ class BaseDatabaseManagement(object):
         self.dbmanalog.critical("Please set the BLAST environment variables in your .bash_profile!!")
         # TODO-ROB:  set up environment variables.  Also add CLI setup
 
-    def download_taxonomy_database(self, db_type, sub_path):
-        """
-        This method gets the remote data and updates the local databases for ETE3, BioSQL, and PhyloDB taxonomy
-        databases.  Most significant is the "biosql" and "phylodb" types.  The biosql databases use NCBI's taxonomy
-        database along with the biosql schema.  And the phylodb databases use ITIS's taxonomy database along with the
-        biosql schema.
+    def download_ete3_taxonomy_database(self):
+        # DEFAULT_TAXADB = os.path.join(os.environ.get('HOME', '/'), '.etetoolkit', 'taxa.sqlite')
+        ete3 = import_module("ete3")
+        ete3.NCBITaxa.update_taxonomy_database()
 
-        :param db_type:  The type of database.  ("ete3", "biosql", or "phylodb")
-        :param dest_name:  The name of the new database.
-        :param dest_path:  The location where the new database should go.
-        :return:  An updated taxonomy database.
-        """
-        db_type = str(db_type).lower()
-        if db_type == 'ete3':
-            # DEFAULT_TAXADB = os.path.join(os.environ.get('HOME', '/'), '.etetoolkit', 'taxa.sqlite')
-            ete3 = import_module("ete3")
-            ete3.NCBITaxa.update_taxonomy_database()
-        elif db_type == 'biosql':
-            # Loads data from NCBI via ftp://ftp.ncbi.nlm.nih.gov/pub/taxonomy
-            if self.driver.lower() == "sqlite3":
-                ncbi_db = self.biosql.SQLiteBioSQL(proj_mana=self.proj_mana)
-                ncbi_db.copy_template_database(sub_path=sub_path)
-                return ncbi_db
+    def download_biosql_taxonomy_database(self):
+        if self.driver.lower() == "sqlite3":
+            ncbi_db = self.biosql.SQLiteBioSQL(proj_mana=self.proj_mana)
+            ncbi_db.copy_template_database(destination=self.database_path)
+            return ncbi_db
 
-            elif self.driver.lower() == "mysql":
-                db_path = self.ncbi_db_repo / Path('pub') / Path('taxonomy')
-                ncbi_db = self.biosql.MySQLBioSQL()
-                return ncbi_db
+        elif self.driver.lower() == "mysql":
+            db_path = self.ncbi_db_repo / Path('pub') / Path('taxonomy')
+            ncbi_db = self.biosql.MySQLBioSQL()
+            return ncbi_db
 
-        elif db_type == 'phylodb':
-            # Loads data from ITIS via http://www.itis.gov/downloads/
-            print('biosql_repo')
+    def download_itis_taxonomy_tables(self):
+        # Loads data from ITIS via http://www.itis.gov/downloads/
+        # Use this along with BioSQL's phyloDB
+        pass
 
     def download_ncbi_taxonomy_dump_files(self, url="ftp://ftp.ncbi.nlm.nih.gov/pub/taxonomy/taxdump.tar.gz"):
+        # TODO-ROB:  Maybe add taxdata folder here to reflect BioSQL's load_ncbi_taxonomy.pl script
         dl_path = Path(self.database_path) / Path("NCBI") / Path('pub') / Path('taxonomy')
         dl_abs_path = dl_path / Path('taxdump.tar.gz')
         r = requests.get(url, allow_redirects=True)
         with open(str(dl_abs_path), 'wb') as taxdump:
             taxdump.write(r.content)
+        with tarfile.open(str(dl_abs_path)) as tar:
+            tar.extractall(dl_path)
 
     # TODO-ROB:  Update ncbiftp class syntax to reflect NCBI's ftp site
     def download_refseq_release_files(self, collection_subset, seqtype, filetype):
@@ -119,7 +111,7 @@ class BaseDatabaseManagement(object):
     def upload_refseq_release_files(self, collection_subset, seqtype, filetype, upload_list, extension=".gbk.db"):
         db_path = self.database_path / Path("NCBI") / Path("refseq") / Path("release") / Path(collection_subset)
         # Get a BioSQL database
-        ncbi_db = self.download_taxonomy_database(db_type="biosql", sub_path="/refseq/release/%s" % collection_subset)
+        ncbi_db = self.download_biosql_taxonomy_database(destination="/refseq/release/%s" % collection_subset)
         ncbi_db.upload_path = db_path
         ncbi_db.upload_list = upload_list
         ncbi_db.upload_files(seqtype=seqtype, filetype=filetype)
@@ -128,6 +120,39 @@ class BaseDatabaseManagement(object):
     def get_project_genbank_database(self):
         """"""
         print()
+
+    # def download_taxonomy_database(self, db_type, destination):
+    #     """
+    #     This method gets the remote data and updates the local databases for ETE3, BioSQL, and PhyloDB taxonomy
+    #     databases.  Most significant is the "biosql" and "phylodb" types.  The biosql databases use NCBI's taxonomy
+    #     database along with the biosql schema.  And the phylodb databases use ITIS's taxonomy database along with the
+    #     biosql schema.
+    #
+    #     :param db_type:  The type of database.  ("ete3", "biosql", or "phylodb")
+    #     :param dest_name:  The name of the new database.
+    #     :param dest_path:  The location where the new database should go.
+    #     :return:  An updated taxonomy database.
+    #     """
+        # db_type = str(db_type).lower()
+        # if db_type == 'ete3':
+        #     # DEFAULT_TAXADB = os.path.join(os.environ.get('HOME', '/'), '.etetoolkit', 'taxa.sqlite')
+        #     ete3 = import_module("ete3")
+        #     ete3.NCBITaxa.update_taxonomy_database()
+        # elif db_type == 'biosql':
+        #     # Loads data from NCBI via ftp://ftp.ncbi.nlm.nih.gov/pub/taxonomy
+        #     if self.driver.lower() == "sqlite3":
+        #         ncbi_db = self.biosql.SQLiteBioSQL(proj_mana=self.proj_mana)
+        #         ncbi_db.copy_template_database(destination=destination)
+        #         return ncbi_db
+        #
+        #     elif self.driver.lower() == "mysql":
+        #         db_path = self.ncbi_db_repo / Path('pub') / Path('taxonomy')
+        #         ncbi_db = self.biosql.MySQLBioSQL()
+        #         return ncbi_db
+
+        # elif db_type == 'phylodb':
+        #     # Loads data from ITIS via http://www.itis.gov/downloads/
+        #     print('biosql_repo')
 
 
 class DatabaseManagement(BaseDatabaseManagement):
