96c82ed7880c7871d882f95db26962d404bf8f62
sdhutchins <sdhutchins@users.noreply.github.com>
2017-11-15 11:42:12 -0600

    Updated mygene/__init__.py and Tools/__init__.py

--- Datasnakes/Tools/__init__.py
+++ Datasnakes/Tools/__init__.py
@@ -5,3 +5,4 @@ from Datasnakes.Tools.slackify import Slackify
 from Datasnakes.Tools.utils import (formatlist, splitlist, makedirectory,
                                     PackageVersion)
 from Datasnakes.Tools.ftp import BaseFTPClient, NcbiFTPClient
+from Datasnakes.Tools.mygene import MyGene
