491a1b6b2f9d14946dd5ed26438dd3a34a4930cd
sdhutchins <sdhutchins@users.noreply.github.com>
2017-11-17 17:44:50 -0600

    Updated conf.py

--- Docs/docs/source/conf.py
+++ Docs/docs/source/conf.py
@@ -158,7 +158,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, 'datasnakes-scripts', 'OrthoEvolution Documentation',
+    (master_doc, 'OrthoEvolution', 'OrthoEvolution Documentation',
      [author], 1)
 ]
 
