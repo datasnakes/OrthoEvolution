8f3eecd61ca110a4e946b9dfca91fc4118dd0bca
sdhutchins <sdhutchins@users.noreply.github.com>
2017-11-17 17:59:55 -0600

    Removed rst_prolog

--- Docs/docs/source/conf.py
+++ Docs/docs/source/conf.py
@@ -199,8 +199,3 @@ epub_exclude_files = ['search.html']
 
 # Example configuration for intersphinx: refer to the Python standard library.
 intersphinx_mapping = {'https://docs.python.org/': None}
-
-
-rst_prolog = """
-.. |exclamation| replace:: ❗
-"""
