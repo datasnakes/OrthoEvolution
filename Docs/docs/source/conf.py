b76909b3fdc31d1d6cdc0c7622536a29bc8b1045
sdhutchins <sdhutchins@users.noreply.github.com>
2017-11-20 18:54:22 -0600

    Updated conf.py

--- Docs/docs/source/conf.py
+++ Docs/docs/source/conf.py
@@ -19,7 +19,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('modules' + os.sep))
+sys.path.insert(0, os.path.abspath('modules'))
 
 
 # -- General configuration ------------------------------------------------
