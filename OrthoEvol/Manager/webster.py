16befd7ec1c99b3fc91e2a8400e4b414556ed41c
sdhutchins <sdhutchins@users.noreply.github.com>
2017-11-27 16:06:15 -0600

    Updated webster.py docstring

--- OrthoEvol/Manager/webster.py
+++ OrthoEvol/Manager/webster.py
@@ -8,6 +8,7 @@ from pkg_resources import resource_filename
 
 # Based off of https://github.com/etetoolkit/ete/blob/master/ete3/citation.py
 class Webster(object):
+    """Webster is a class of commonly used dictionaries for OrthoEvol."""
 
     archive_options = {
         "Full": Path(''),
@@ -68,6 +69,11 @@ class Webster(object):
         self.citations = set()
 
     def add(self, ref):
+        """Add a reference.
+
+        :param ref: A reference dictionary.
+        :return:
+        """
         self.citations.add(self.reference_options[ref])
 
     def show(self):
