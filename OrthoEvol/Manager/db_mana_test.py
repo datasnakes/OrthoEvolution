ba2d5db2f1f071b65185f7734db6b69951dfd60a
rgilmore <rgilmore@umc.edu>
2018-01-12 17:50:50 -0600

    Added a file used to successfully test the database dispatching functionality.

---
+++ OrthoEvol/Manager/db_mana_test.py
@@ -0,0 +1,17 @@
+from OrthoEvol.Manager.management import ProjectManagement
+from OrthoEvol.Manager.database_dispatcher import DatabaseDispatcher
+from OrthoEvol.Manager.config import yml
+from pkg_resources import resource_filename
+import yaml
+
+db_config_file = resource_filename(yml.__name__, "database_config.yml")
+pm_config_file = resource_filename(yml.__name__, "config_template_existing.yml")
+with open(pm_config_file, 'r') as f:
+    pm_config = yaml.safe_load(f)
+pm = ProjectManagement(**pm_config["Management_config"])
+
+dd = DatabaseDispatcher(db_config_file, pm)
+
+for strategy in dd.strategies:
+    print(strategy)
+    dd.dispatch(strategy, dd.dispatcher, dd.configuration)
