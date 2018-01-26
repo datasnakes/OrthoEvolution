cc38f57cd6c18eadac91f393fb36bbb6b4a15f16
sdhutchins <sdhutchins@outlook.com>
2018-01-26 17:25:15 -0600

    Updated blast test with new method.

--- OrthoEvol/Orthologs/Blast/test.py
+++ OrthoEvol/Orthologs/Blast/test.py
@@ -14,5 +14,6 @@ Blast_config = {
 
 
 path = os.getcwd()
-myblast = BaseBlastN(proj_mana=None, project="sdh-test", blast_method=None, project_path=path, **Blast_config)
+myblast = BaseBlastN(proj_mana=None, project="sdh-test", blast_method=3,
+                     project_path=path, **Blast_config)
 myblast.blast_config(myblast.blast_human, 'Homo_sapiens', auto_start=True)
