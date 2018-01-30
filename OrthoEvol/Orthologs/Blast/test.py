2af9bd528fb41cca131093ddd7cde191b22ddd69
sdhutchins <sdhutchins@users.noreply.github.com>
2018-01-29 18:56:04 -0600

    Simplification of example

--- OrthoEvol/Orthologs/Blast/test.py
+++ OrthoEvol/Orthologs/Blast/test.py
@@ -2,18 +2,20 @@ from OrthoEvol.Orthologs.Blast import OrthoBlastN, BaseBlastN
 import os
 
 # This is more pythonic with YAML loading
-Blast_config = {
-  "taxon_file": None,
-  "go_list": None,
-  "post_blast": True,
-  "template": None,
-  "save_data": True,
-  "copy_from_package": True,
-  "MAF": 'test_blast.csv'
+blastconfig = {
+    "project": "sdh-test",
+    "blast_method": 3,
+    "taxon_file": None,
+    "go_list": None,
+    "post_blast": True,
+    "template": None,
+    "save_data": True,
+    "copy_from_package": True,
+    "MAF": 'test_blast.csv',
+    "project_path": None,
+    "proj_mana": None
 }
 
 
-path = os.getcwd()
-myblast = BaseBlastN(proj_mana=None, project="sdh-test", blast_method=3,
-                     project_path=path, **Blast_config)
+myblast = BaseBlastN(**blastconfig)
 myblast.blast_config(myblast.blast_human, 'Homo_sapiens', auto_start=True)
