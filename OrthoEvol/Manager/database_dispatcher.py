df41bf69d87c9588b6e6728ff52b6cb00ad87c0b
Rob Gilmore <robgilmore127@gmail.com>
2019-05-09 15:10:46 -0500

    Removed commented code in database_dispatcher.

--- OrthoEvol/Manager/database_dispatcher.py
+++ OrthoEvol/Manager/database_dispatcher.py
@@ -8,20 +8,6 @@ class DatabaseDispatcher(DatabaseManagement):
         self.dispatcher, self.configuration = self.get_strategy_dispatcher(db_config_strategy=self.db_config_strategy)
         self.strategies = list(self.dispatcher.keys())
         self.actions = ["archive", "upload", "configure", "delete"]
-        # if len(self.dispatcher.keys()) == 1:
-        #     self.strategy = list(self.dispatcher.keys())[0]
-        #
-        #     # Create class methods from dispatcher
-        #     self.archive_disp = self.dispatcher[self.strategy]["archive"]
-        #     self.configure_disp = self.dispatcher[self.strategy]["configure"]
-        #     self.upload_disp = self.dispatcher[self.strategy]["upload"]
-        #
-        #     # Create class attributes from configuration
-        #     self.archive_config = self.configuration[self.strategy]["archive"]
-        #     self.configure_config = self.configuration[self.strategy]["configure"]
-        #     self.upload_config = self.configuration[self.strategy]["configure"]
-        # else:
-        #     for disp, c in zip(self.dispatcher, self.configuration):
 
         if upload_refseq_release == True:
             self.refseq_release_dispatcher(**kwargs)
