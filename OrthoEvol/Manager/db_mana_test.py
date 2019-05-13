580a59a25320b0733d4f7b28f427433a064848b1
Rob Gilmore <robgilmore127@gmail.com>
2019-05-13 15:55:20 -0500

    Updated database management test script.

--- OrthoEvol/Manager/db_mana_test.py
+++ OrthoEvol/Manager/db_mana_test.py
@@ -5,17 +5,18 @@ from pkg_resources import resource_filename
 from pathlib import Path
 import yaml
 
-# Set up configuration data using OrthoEvol config files
-db_config_file = resource_filename(yml.__name__, "databases.yml")
+# Set up project management
 pm_config_file = resource_filename(yml.__name__, "initialize_new.yml")
 with open(pm_config_file, 'r') as f:
     pm_config = yaml.load(f)
+pm = ProjectManagement(**pm_config["Management_config"])
+
+# Set up database management
+db_config_file = resource_filename(yml.__name__, "databases.yml")
 with open(db_config_file, 'r') as f:
     db_config = yaml.load(f)
 config = db_config.update(pm_config)
 
-# Set up project management
-pm = ProjectManagement(**pm_config["Management_config"])
 # Generate main config file for this job
 config_file = pm.user_log / Path("upload_config.yml")
 with open(str(config_file), 'w') as cf:
