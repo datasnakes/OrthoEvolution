caa689299fcb6bcc323571e52769c24ec4021fbb
Rob Gilmore <robgilmore127@gmail.com>
2019-05-13 15:15:47 -0500

    Updated database management test script.

--- OrthoEvol/Manager/db_mana_test.py
+++ OrthoEvol/Manager/db_mana_test.py
@@ -2,14 +2,25 @@ from OrthoEvol.Manager.management import ProjectManagement
 from OrthoEvol.Manager.database_dispatcher import DatabaseDispatcher
 from OrthoEvol.Manager.config import yml
 from pkg_resources import resource_filename
+from pathlib import Path
 import yaml
 
+# Set up configuration data using OrthoEvol config files
 db_config_file = resource_filename(yml.__name__, "databases.yml")
-pm_config_file = resource_filename(yml.__name__, "config_template_existing.yml")
+pm_config_file = resource_filename(yml.__name__, "initialize_new.yml")
 with open(pm_config_file, 'r') as f:
-    pm_config = yaml.safe_load(f)
+    pm_config = yaml.load(f)
+with open(db_config_file, 'r') as f:
+    db_config = yaml.load(f)
+config = db_config.update(pm_config)
+
+# Set up project management
 pm = ProjectManagement(**pm_config["Management_config"])
+# Generate main config file for this job
+config_file = pm.user_log / Path("upload_config.yml")
+with open(str(config_file), 'w') as cf:
+    yaml.dump(config, cf, default_flow_style=False)
 
-dd = DatabaseDispatcher(db_config_file, pm)
-
+# Set up database dispatcher and dispatch the functions
+dd = DatabaseDispatcher(config_file, pm)
 dd.dispatch(dd.strategies, dd.dispatcher, dd.configuration)
