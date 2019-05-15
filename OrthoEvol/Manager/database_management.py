abfd7958b66a21cc49fbb2a866e2c09e39047c90
Rob Gilmore <robgilmore127@gmail.com>
2019-05-15 16:02:23 -0500

    Updated automation for template generation and downloading refseq files.

--- OrthoEvol/Manager/database_management.py
+++ OrthoEvol/Manager/database_management.py
@@ -646,7 +646,7 @@ class DatabaseManagement(BaseDatabaseManagement):
 
     def NCBI_refseq_release(self, configure_flag=None, archive_flag=None, delete_flag=None, upload_flag=None, archive_path=None,
                             database_path=None, collection_subset=None, seqtype=None, seqformat=None, file_list=None,
-                            upload_number=8, _path=None, activate=None, configure_template=None):
+                            upload_number=8, _path=None, activate=None, configure_template=None, download_flag=None):
         """
         This is the most complicated specific strategy.  It downloads the refseq release files of choice (gbff),
         extracts the data, splits a list of files into {upload_number} lists, and uploads those file lists to
@@ -700,16 +700,22 @@ class DatabaseManagement(BaseDatabaseManagement):
                 "delete_flag": delete_flag
             })
         if configure_flag:
-            nrr_dispatcher["NCBI_refseq_release"]["configure"].append(self.download_refseq_release_files)
-            nrr_config["NCBI_refseq_release"]["configure"].append({
-                "collection_subset": collection_subset,
-                "seqtype": seqtype,
-                "seqformat": seqformat
-            })
-        if configure_template:
-            nrr_dispatcher["NCBI_refseq_release"]["configure"].append(self.create_biosql_taxonomy_template)
-            nrr_config["NCBI_refseq_release"]["configure"].append({})
+            if download_flag:
+                nrr_dispatcher["NCBI_refseq_release"]["configure"].append(self.download_refseq_release_files)
+                nrr_config["NCBI_refseq_release"]["configure"].append({
+                    "collection_subset": collection_subset,
+                    "seqtype": seqtype,
+                    "seqformat": seqformat
+                })
+            if configure_template:
+                nrr_dispatcher["NCBI_refseq_release"]["configure"].append(self.create_biosql_taxonomy_template)
+                nrr_config["NCBI_refseq_release"]["configure"].append({})
+
         if upload_flag:
+            if not _biosql.template_abs_path.is_file():
+                nrr_dispatcher["NCBI_refseq_release"]["configure"].append(self.create_biosql_taxonomy_template)
+                nrr_config["NCBI_refseq_release"]["configure"].append({})
+
             if upload_number < 8:
                 raise ValueError("The upload_number must be greater than 8.  The NCBI refseq release files are too bing"
                                  "for anything less than 8 seperate BioSQL databases.")
