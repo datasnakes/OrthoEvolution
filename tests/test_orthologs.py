aead7a2a70808989c9712f3b2142c1c38aac4eea
Shaurita <ms.s.hutchins@gmail.com>
2019-05-31 18:31:35 -0500

    Added a test for OrthoBlastN.

--- tests/test_orthologs.py
+++ tests/test_orthologs.py
@@ -1,23 +1,29 @@
 """This is the test suite for Orthologs."""
+import unittest
+from shutil import rmtree
 
-#from OrthoEvol.Orthologs.Blast import CompGenBLASTn
-#import unittest
-#
-#class TestOrthologs(unittest.TestCase):
-#    """Test the Orthologs module."""
-#    def test_phylo(self):
-#        """Test the Phylogenetics modules."""
-#    def test_align():
-#        """Test the Align module."""
-#    def test_compgenetics():
-#        """Test the Comparative Genetics module."""
-#    def test_genbank():
-#        """Test the GenBank module."""
-#    def test_blast():
-#        """Test the Blast module."""
-#    def test_biosql():
-#        """Test the BioSQL module."""
+from OrthoEvol.Orthologs.Blast import OrthoBlastN
 
 
+class TestOrthologs(unittest.TestCase):
+    """Test the Orthologs module."""
 
-# TODO-SDH Write tests for Orthologs module using unit tests.
+    def setUp(self, project="orthology-gpcr"):
+        self.project = project
+
+    def delete_project(self, project_path):
+        rmtree(project_path)
+
+    def test_orthoblastn(self):
+        """Test the OrthoBlastN class."""
+        with self.assertRaises(EnvironmentError):
+            gpcr_blastn = OrthoBlastN(project=self.project, method=3,
+                                      save_data=True, acc_file="gpcr.csv",
+                                      copy_from_package=True)
+            self.assertEqual(gpcr_blastn.acc_file, "gpcr.csv")
+            self.assertTrue(gpcr_blastn.copy_from_package)
+        self.delete_project(project_path=self.project)
+
+
+if __name__ == '__main__':
+    unittest.main()
