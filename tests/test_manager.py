8aae40ecbc6ad3500c8aa437f8442e6e4b03362f
sdhutchins <sdhutchins@outlook.com>
2019-06-05 18:15:21 -0500

    Added BaseBlastN test.

--- tests/test_manager.py
+++ tests/test_manager.py
@@ -8,23 +8,23 @@ from OrthoEvol.Manager.management import ProjectManagement
 class TestManager(unittest.TestCase):
     """Test the Manager module."""
 
-    def setUp(self, project='test-project', repository='test-repo'):
+    def setUp(self, project='test-project', repository=None):
         self.project = project
         self.repo = repository
 
-    def delete_repo(self):
-        rmtree(self.repo)
+    def delete_project(self):
+        rmtree(self.project)
 
     def test_projectmanagement(self):
         """Test the ProjectManagement class."""
-        ProjectManagement(repo=self.repo, user='test-user',
+        ProjectManagement(repo=self.repo, user=None,
                           project=self.project,
-                          research='test-research',
+                          research=None,
                           research_type='comparative_genetics',
-                          new_repo=True, new_user=True, new_project=True,
-                          new_research=True)
-        self.assertEqual(str(self.repo), 'test-repo')
-        self.delete_repo()
+                          new_repo=False, new_user=False, new_project=True,
+                          new_research=False)
+        self.assertEqual(str(self.project), 'test-project')
+        self.delete_project()
 
 
 if __name__ == '__main__':
--- tests/test_orthologs.py
+++ tests/test_orthologs.py
@@ -2,27 +2,34 @@
 import unittest
 from shutil import rmtree
 
-from OrthoEvol.Orthologs.Blast import OrthoBlastN
+from OrthoEvol.Orthologs.Blast import BaseBlastN
 
 
 class TestOrthologs(unittest.TestCase):
     """Test the Orthologs module."""
 
-    def setUp(self, project="orthology-gpcr"):
+    def setUp(self, project="gpcr", project_path="projects"):
         self.project = project
+        self.project_path = project_path
 
     def delete_project(self, project_path):
         rmtree(project_path)
 
-    def test_orthoblastn(self):
-        """Test the OrthoBlastN class."""
+    def test_baseblastn(self):
+        """Test the BaseBlastN class."""
+        # The with statement is for travisci where a BLASTDB variable
+        # is not set.
+        # TIP: Remove the with statement if testing locally.
         with self.assertRaises(EnvironmentError):
-            gpcr_blastn = OrthoBlastN(project=self.project, method=3,
-                                      save_data=True, acc_file=None,
-                                      copy_from_package=True)
-            self.assertEqual(gpcr_blastn.acc_file, None)
+            gpcr_blastn = BaseBlastN(project=self.project, method=3,
+                                     save_data=True, acc_file="gpcr.csv",
+                                     proj_mana=None,
+                                     project_path=self.project_path,
+                                     copy_from_package=True)
+            self.assertEqual(gpcr_blastn.proj_mana, None)
+            self.assertEqual(gpcr_blastn.acc_file, "gpcr.csv")
             self.assertTrue(gpcr_blastn.copy_from_package)
-        self.delete_project(project_path=self.project)
+            self.delete_project(project_path=self.project_path)
 
 
 if __name__ == '__main__':
