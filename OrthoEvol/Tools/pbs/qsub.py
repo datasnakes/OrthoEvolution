98840a2bdbe1244385d15895527c845766f58825
Rob Gilmore <robgilmore127@gmail.com>
2019-07-29 17:42:57 -0500

    Fixed subprocess bug.

--- OrthoEvol/Tools/pbs/qsub.py
+++ OrthoEvol/Tools/pbs/qsub.py
@@ -106,7 +106,7 @@ class BaseQsub(object):
 
         try:
             if cmd is None:
-                cmd = ['qsub ' + str(self.pbs_script)]  # this is the command
+                cmd = ['qsub', str(self.pbs_script)]  # this is the command
             # Shell MUST be True
             proc = self.qsub_utils.system_cmd(cmd, stdout=sp.PIPE, stderr=sp.PIPE, **kwargs)
         except sp.CalledProcessError as err:
