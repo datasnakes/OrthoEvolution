e3a14642015dd5c4531ce08b6e263abbb240b793
sdhutchins <sdhutchins@outlook.com>
2019-08-27 13:25:25 -0500

    Added docstrings.

--- OrthoEvol/Orthologs/Phylogenetics/TreeViz/treeviz.py
+++ OrthoEvol/Orthologs/Phylogenetics/TreeViz/treeviz.py
@@ -3,6 +3,7 @@ import warnings
 
 from Bio import Phylo
 from ete3 import Tree
+import matplotlib.pyplot as plt
 
 from OrthoEvol.Orthologs import OrthologsDevelopmentWarning
 
@@ -16,7 +17,8 @@ class TreeViz(object):
         :param path:  The path to your tree file.
         :type path: str
         :param tree_format:  The format of the tree, default value = 'newick'
-        :type path: tree_format
+        :type tree_format: str
+        :return: A Bio.Phylo tree object
         """
         # Warn users about this module
         warnings.warn('This module is still under development and '
@@ -32,17 +34,29 @@ class TreeViz(object):
 
         :param path: The path to your tree file.
         :type path: str
-        :param tree_format: The format of the tree, default value = 'newick'
+        :param tree_format: The format of the tree, defaults to "newick"
         :type tree_format: str
         """
         tree = Phylo.read(file=self.path, format=self.tree_format)
         return tree
 
-    def draw_tree(self, drawing_type="default"):
-        """Import a newick formatted tree and visualize it."""
+    def draw_tree(self, drawing_type="default", auto_show=False):
+        """Import a newick formatted tree and visualize it.
+
+        :param drawing_type: The type of drawing to create, defaults to "default"
+        :type drawing_type: str, optional
+        """
         if drawing_type == "ascii":
             Phylo.draw_ascii(self.tree)
         elif drawing_type == "graphviz":
             Phylo.draw_graphviz(self.tree)
         elif drawing_type == "default":
-            Phylo.draw(self.tree)
+            Phylo.draw(tree=self.tree, do_show=auto_show)
+
+    def save_tree(self, filename):
+        """Save the tree image.
+
+        :param filename: The name of the image file.
+        :type filename: str
+        """
+        plt.savefig(fname=filename)
