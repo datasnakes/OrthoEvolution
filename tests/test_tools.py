698b4efdf5f6263a53ac8cd8054ff99b303346a0
Shaurita Hutchins <shaurita.d.hutchins@gmail.com>
2020-07-20 15:02:11 -0500

    Added a mygene test.

--- tests/test_tools.py
+++ tests/test_tools.py
@@ -2,10 +2,13 @@
 
 import unittest
 import os
+from pkg_resources import resource_filename
 
 from OrthoEvol.Tools.logit import LogIt
 from OrthoEvol.Tools.parallel import Multiprocess
 from OrthoEvol.Tools.ftp import NcbiFTPClient
+from OrthoEvol.Tools.mygene import MyGene
+from OrthoEvol.Manager.config import test
 
 
 class TestTools(unittest.TestCase):
@@ -13,6 +16,8 @@ class TestTools(unittest.TestCase):
 
     def setUp(self, logfile='test.log'):
         self.logfile = logfile
+        self.infile = resource_filename(test.__name__, 'test_blast.csv')
+        self.outfile = 'out_mygene.csv'
 
     def test_logit(self):
         """Test the LogIt class."""
@@ -33,6 +38,15 @@ class TestTools(unittest.TestCase):
         ncbiftp = NcbiFTPClient(self.assertTrue('someone@gmail.com'))
         self.assertIsNotNone(ncbiftp.ftp.welcome)
         ncbiftp.close_connection()
+
+    def test_mygene(self):
+        """Test the MyGene class."""
+        mg = MyGene(infile=self.infile, outfile=self.outfile)
+        self.assertEqual(mg.species, 'human')
+        self.assertIsNotNone(mg.accessions_list)
+        self.assertIsInstance(mg.accessions_list, list)
+        mg.query_mygene()
+        os.remove(self.outfile)
 
 
 if __name__ == '__main__':
