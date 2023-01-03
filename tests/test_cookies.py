963ab83fb30baba5e536df9cfcf4c261f6498b5a
sdhutchins <sdhutchins@uab.edu>
2023-01-03 01:44:43 -0600

    Add more test infrastructure.

---
+++ tests/test_cookies.py
@@ -0,0 +1,36 @@
+import unittest
+import os
+
+from OrthoEvol.Cookies import CookBook, Oven
+
+class TestCookBookOven(unittest.TestCase):
+    def test_cookbook_init(self):
+        # Create a new instance of the CookBook class
+        cookbook = CookBook()
+
+        # Check that the attributes of the CookBook class are correctly initialized
+        self.assertTrue(hasattr(cookbook, "CookieJar"))
+        self.assertTrue(hasattr(cookbook, "repo_cookie"))
+        self.assertTrue(hasattr(cookbook, "user_cookie"))
+        self.assertTrue(hasattr(cookbook, "project_cookie"))
+        self.assertTrue(hasattr(cookbook, "basic_project_cookie"))
+        self.assertTrue(hasattr(cookbook, "research_cookie"))
+        self.assertTrue(hasattr(cookbook, "app_cookie"))
+        self.assertTrue(hasattr(cookbook, "db_cookie"))
+        self.assertTrue(hasattr(cookbook, "website_cookie"))
+        self.assertTrue(hasattr(cookbook, "CONFIGURATION"))
+
+    def test_oven_bake_cookies(self):
+        # Create a new instance of the Oven class
+        oven = Oven()
+
+        # Set the output directory for the baked cookies
+        cookie_jar = "test_cookie_jar"
+        oven.cookie_jar = cookie_jar
+
+        # Bake a cookie and check that a new directory was created in the output directory
+        oven.bake_cookies(recipe="repo_cookie", repo="test_repo")
+        self.assertTrue(os.path.exists(os.path.join(cookie_jar, "test_repo")))
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ tests/test_utils.py
@@ -0,0 +1,58 @@
+import unittest
+import os
+import time
+import shutil
+import tempfile
+
+from OrthoEvol.utilities import FunctionRepeater, CookieUtils
+
+
+class TestUtils(unittest.TestCase):
+    """Test the Tools module."""
+
+    def test_function_repeater(self):
+        # Create a dummy function that increments a counter
+        counter = 0
+        def increment():
+            nonlocal counter
+            counter += 1
+        
+        # Create a FunctionRepeater instance that runs the dummy function every 1 second
+        repeater = FunctionRepeater(1, increment)
+        
+        # Wait for 2 seconds
+        time.sleep(2)
+        
+        # Stop the repeater
+        repeater.stop()
+        
+        # Check that the dummy function was run at least twice
+        assert counter >= 2
+
+    def test_archive(self):
+        # Create a temporary directory to use as the database path
+        database_path = tempfile.mkdtemp()
+        # Create a file in the database path
+        with open(os.path.join(database_path, "test.txt"), "w") as f:
+            f.write("Test content")
+        # Create a temporary directory to use as the archive path
+        archive_path = tempfile.mkdtemp()
+        
+        # Create a CookieUtils instance
+        utils = CookieUtils()
+        
+        # Run the archive method with the "Full" option
+        utils.archive(database_path, archive_path, "Full")
+        
+        # Check that the file has been archived in a .tar.xz file
+        archived_files = os.listdir(archive_path)
+        assert len(archived_files) == 1
+        assert archived_files[0].endswith(".tar.xz")
+        
+        # Clean up
+        shutil.rmtree(database_path)
+        shutil.rmtree(archive_path)
+
+
+if __name__ == '__main__':
+    unittest.main()
